c6b11f419004cfd5c3d68e56fc9ef82c10cc35b8
Richard Henderson <richard.henderson@linaro.org>
2021-03-13 12:29:28 -0600

    tcg: Move in_code_gen_buffer and tests to region.c
    Shortly, the full code_gen_buffer will only be visible
    to region.c, so move in_code_gen_buffer out-of-line.
    
    Move the debugging versions of tcg_splitwx_to_{rx,rw}
    to region.c as well, so that the compiler gets to see
    the implementation of in_code_gen_buffer.
    
    This leaves exactly one use of in_code_gen_buffer outside
    of region.c, in cpu_restore_state.  Which, being on the
    exception path, is not performance critical.
    
    Reviewed-by: Alex Bennée <alex.bennee@linaro.org>
    Reviewed-by: Luis Pires <luis.pires@eldorado.org.br>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- include/tcg/tcg.h
+++ include/tcg/tcg.h
@@ -747,16 +747,7 @@ extern TCGv _pc_is_current;
 extern TCGv_i32 cpu_rvfi_available_fields;
 #endif
 
-static inline bool in_code_gen_buffer(const void *p)
-{
-    const TCGContext *s = &tcg_init_ctx;
-    /*
-     * Much like it is valid to have a pointer to the byte past the
-     * end of an array (so long as you don't dereference it), allow
-     * a pointer to the byte past the end of the code gen buffer.
-     */
-    return (size_t)(p - s->code_gen_buffer) <= s->code_gen_buffer_size;
-}
+bool in_code_gen_buffer(const void *p);
 
 #ifdef CONFIG_DEBUG_TCG
 const void *tcg_splitwx_to_rx(void *rw);
