2578c4233d395f01036de07dcf34c31502abaea3
Alistair Francis <alistair.francis@wdc.com>
2021-04-24 13:29:50 +1000

    target/riscv: Remove the hardcoded SSTATUS_SD macro
    This also ensures that the SD bit is not writable.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Message-id: 9ea842309f0fd7adff172790f5b5fc058b40f2f1.1619234854.git.alistair.francis@wdc.com

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -534,11 +534,11 @@ static const target_ulong delegable_excps =
     0;
 static const target_ulong sstatus_v1_10_mask = SSTATUS_SIE | SSTATUS_SPIE |
     SSTATUS_UIE | SSTATUS_UPIE | SSTATUS_SPP | SSTATUS_FS | SSTATUS_XS |
-    SSTATUS_SUM | SSTATUS_MXR |
+    SSTATUS_SUM | SSTATUS_MXR
 #if defined(TARGET_RISCV64)
-    SSTATUS64_UXL |
+    | SSTATUS64_UXL
 #endif
-    SSTATUS_SD;
+    ;
 static const target_ulong sip_writable_mask = SIP_SSIP | MIP_USIP | MIP_UEIP;
 static const target_ulong hip_writable_mask = MIP_VSSIP;
 static const target_ulong hvip_writable_mask = MIP_VSSIP | MIP_VSTIP | MIP_VSEIP;
@@ -831,6 +831,13 @@ static RISCVException read_sstatus(CPURISCVState *env, int csrno,
                                    target_ulong *val)
 {
     target_ulong mask = (sstatus_v1_10_mask);
+
+    if (riscv_cpu_is_32bit(env)) {
+        mask |= SSTATUS32_SD;
+    } else {
+        mask |= SSTATUS64_SD;
+    }
+
     *val = env->mstatus & mask;
     return RISCV_EXCP_NONE;
 }
