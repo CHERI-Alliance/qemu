402a681ef97dac8e503125492ece95df1efa2acb
Frank Chang <frank.chang@sifive.com>
2021-05-06 00:06:14 +0800

    target/riscv: rvb: generalized or-combine
    Signed-off-by: Frank Chang <frank.chang@sifive.com>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20210505160620.15723-14-frank.chang@sifive.com
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/helper.h
+++ target/riscv/helper.h
@@ -77,6 +77,8 @@ DEF_HELPER_FLAGS_4(riscv_log_instr, TCG_CALL_NO_RWG, void, env, tl, i32, i32)
 /* Bitmanip */
 DEF_HELPER_FLAGS_2(grev, TCG_CALL_NO_RWG_SE, tl, tl, tl)
 DEF_HELPER_FLAGS_2(grevw, TCG_CALL_NO_RWG_SE, tl, tl, tl)
+DEF_HELPER_FLAGS_2(gorc, TCG_CALL_NO_RWG_SE, tl, tl, tl)
+DEF_HELPER_FLAGS_2(gorcw, TCG_CALL_NO_RWG_SE, tl, tl, tl)
 
 /* Special functions */
 DEF_HELPER_3(csrrw, tl, env, tl, tl)
