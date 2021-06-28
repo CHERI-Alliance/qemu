a316b6f72422ad3db304c821b037cc2c997d81ef
Peter Maydell <peter.maydell@linaro.org>
2021-06-28 14:58:33 +0100

    target/arm: Implement MVE long shifts by register
    Implement the MVE long shifts by register, which perform shifts on a
    pair of general-purpose registers treated as a 64-bit quantity, with
    the shift count in another general-purpose register, which might be
    either positive or negative.
    
    Like the long-shifts-by-immediate, these encodings sit in the space
    that was previously the UNPREDICTABLE MOVS/ORRS with Rm==13,15.
    Because LSLL_rr and ASRL_rr overlap with both MOV_rxri/ORR_rrri and
    also with CSEL (as one of the previously-UNPREDICTABLE Rm==13 cases),
    we have to move the CSEL pattern into the same decodetree group.
    
    Signed-off-by: Peter Maydell <peter.maydell@linaro.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-id: 20210628135835.6690-17-peter.maydell@linaro.org

--- target/arm/translate.h
+++ target/arm/translate.h
@@ -466,6 +466,7 @@ typedef void CryptoThreeOpFn(TCGv_ptr, TCGv_ptr, TCGv_ptr);
 typedef void AtomicThreeOpFn(TCGv_i64, TCGv_cap_checked_ptr, TCGv_i64, TCGArg,
                              MemOp);
 typedef void WideShiftImmFn(TCGv_i64, TCGv_i64, int64_t shift);
+typedef void WideShiftFn(TCGv_i64, TCGv_ptr, TCGv_i64, TCGv_i32);
 
 /**
  * arm_tbflags_from_tb:
