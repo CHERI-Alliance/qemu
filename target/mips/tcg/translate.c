f8d232b5d1db1c0c813be0138c10db146d92dbcd
Philippe Mathieu-Daudé <f4bug@amsat.org>
2021-08-15 16:02:41 +0200

    target/mips: Define gen_helper() macros in translate.h
    To be able to split some code calling the gen_helper() macros
    out of the huge translate.c, we need to define them in the
    'translate.h' local header.
    
    Signed-off-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210816205107.2051495-9-f4bug@amsat.org>

--- target/mips/tcg/translate.c
+++ target/mips/tcg/translate.c
@@ -1455,18 +1455,6 @@ static TCGv cpu_statcounters_icount_kernel, cpu_statcounters_icount_user;
 
 #include "exec/gen-icount.h"
 
-#define gen_helper_0e1i(name, arg1, arg2) do {                    \
-    gen_helper_##name(cpu_env, arg1, tcg_constant_i32(arg2));     \
-    } while (0)
-
-#define gen_helper_1e0i(name, ret, arg1) do {                     \
-    gen_helper_##name(ret, cpu_env, tcg_constant_i32(arg1));      \
-    } while (0)
-
-#define gen_helper_0e2i(name, arg1, arg2, arg3) do {              \
-    gen_helper_##name(cpu_env, arg1, arg2, tcg_constant_i32(arg3));\
-    } while (0)
-
 
 #define gen_helper_2_consti32(name, arg, arg2) do {               \
     TCGv_i32 helper_tmp = tcg_const_i32(arg);                     \
