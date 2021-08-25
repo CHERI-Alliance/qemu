0668bdce347c8beb367ac11c478cd0f09526f233
Thomas Huth <thuth@redhat.com>
2021-08-25 11:20:23 +0200

    softmmu/vl: Deprecate the -sdl and -curses option
    It's not that much complicated to type "-display sdl" or "-display curses",
    so we should not clutter our main option name space with such simple
    wrapper options and rather present the users with a concise interface
    instead. Thus let's deprecate the "-sdl" and "-curses" wrapper options now.
    
    Message-Id: <20210825092023.81396-4-thuth@redhat.com>
    Reviewed-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Peter Krempa <pkrempa@redhat.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -3013,6 +3013,8 @@ void qemu_init(int argc, char **argv, char **envp)
                 dpy.type = DISPLAY_TYPE_NONE;
                 break;
             case QEMU_OPTION_curses:
+                warn_report("-curses is deprecated, "
+                            "use -display curses instead.");
 #ifdef CONFIG_CURSES
                 dpy.type = DISPLAY_TYPE_CURSES;
 #else
@@ -3386,6 +3388,7 @@ void qemu_init(int argc, char **argv, char **envp)
                             "-display ...,window-close=off instead.");
                 break;
             case QEMU_OPTION_sdl:
+                warn_report("-sdl is deprecated, use -display sdl instead.");
 #ifdef CONFIG_SDL
                 dpy.type = DISPLAY_TYPE_SDL;
                 break;
