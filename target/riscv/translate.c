036d8a1214b9232bde3ffaf5b1f4f273b92245ab
Philipp Tomsich <philipp.tomsich@vrull.eu>
2021-09-11 16:00:12 +0200

    target/riscv: Add a REQUIRE_32BIT macro
    With the changes to Zb[abcs], there's some encodings that are
    different in RV64 and RV32 (e.g., for rev8 and zext.h). For these,
    we'll need a helper macro allowing us to select on RV32, as well.
    
    Signed-off-by: Philipp Tomsich <philipp.tomsich@vrull.eu>
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Alistair Francis <alistair.francis@wdc.com>
    Reviewed-by: Bin Meng <bmeng.cn@gmail.com>
    Message-id: 20210911140016.834071-13-philipp.tomsich@vrull.eu
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -528,6 +528,12 @@ EX_SH(12)
     }                              \
 } while (0)
 
+#define REQUIRE_32BIT(ctx) do { \
+    if (!is_32bit(ctx)) {       \
+        return false;           \
+    }                           \
+} while (0)
+
 #define REQUIRE_64BIT(ctx) do { \
     if (is_32bit(ctx)) {        \
         return false;           \
