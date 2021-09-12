cf0220b123b41f78336bcff84d1f4240940c3d69
Richard Henderson <richard.henderson@linaro.org>
2021-09-12 10:49:25 -0700

    tcg/arm: Reduce vector alignment requirement for NEON
    With arm32, the ABI gives us 8-byte alignment for the stack.
    While it's possible to realign the stack to provide 16-byte alignment,
    it's far easier to simply not encode 16-byte alignment in the
    VLD1 and VST1 instructions that we emit.
    
    Remove the assertion in temp_allocate_frame, limit natural alignment
    to the provided stack alignment, and add a comment.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1999878
    Reported-by: Richard W.M. Jones <rjones@redhat.com>
    Reviewed-by: Philippe Mathieu-Daudé <f4bug@amsat.org>
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>
    Message-Id: <20210912174925.200132-1-richard.henderson@linaro.org>
    Message-Id: <20211206191335.230683-2-richard.henderson@linaro.org>

--- tcg/tcg.c
+++ tcg/tcg.c
@@ -3076,7 +3076,13 @@ static void temp_allocate_frame(TCGContext *s, TCGTemp *ts)
         g_assert_not_reached();
     }
 
-    assert(align <= TCG_TARGET_STACK_ALIGN);
+    /*
+     * Assume the stack is sufficiently aligned.
+     * This affects e.g. ARM NEON, where we have 8 byte stack alignment
+     * and do not require 16 byte vector alignment.  This seems slightly
+     * easier than fully parameterizing the above switch statement.
+     */
+    align = MIN(TCG_TARGET_STACK_ALIGN, align);
     off = ROUND_UP(s->current_frame_offset, align);
     assert(off + size <= s->frame_end);
     s->current_frame_offset = off + size;
