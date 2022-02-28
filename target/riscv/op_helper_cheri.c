1ef541f520011a5af9293da79f48480dcb5089a7
Lawrence Esswood <le277@cl.cam.ac.uk>
2022-02-28 14:19:33 +0000

    Use true not 1

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -456,7 +456,7 @@ static target_ulong sc_c_impl(CPUArchState *env, uint32_t addr_reg,
     log_changed_special_reg(env, "load_res", env->load_res, ~0u, 0);
     bool store_fails;
     if (addr != expected_addr) {
-        store_fails = 1;
+        store_fails = true;
         goto sc_failed;
     }
     /*
