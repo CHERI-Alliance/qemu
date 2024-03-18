9e02d2bc9b6d3c4bcb97d7ff6966c5f8e60d57c2
Martin Kaiser <martin.kaiser@codasip.com>
2024-03-18 15:02:02 +0000

    riscv: cheri: remove the UScratchC SCR
    The cheri v9 specification defines a number of SCRs (special capability
    registers). They were removed in the bakewell cheri specification.
    
    One of them is UScratchC (User scratch capability). Remove this register
    from our code.

--- target/riscv/op_helper_cheri.c
+++ target/riscv/op_helper_cheri.c
@@ -82,10 +82,6 @@ struct SCRInfo {
     [CheriSCR_PCC] = {.r = true, .w = false, .access = U_Always, .name = "PCC"},
     [CheriSCR_DDC] = {.r = true, .w = true, .access = U_Always, .name = "DDC"},
 
-    [CheriSCR_UScratchC] = {.r = true,
-                            .w = true,
-                            .access = U_ASR,
-                            .name = "UScratchC"},
     [CheriSCR_UEPCC] = {.r = true, .w = true, .access = U_ASR, .name = "UEPCC"},
 
     [CheriSCR_STCC] = {.r = true, .w = true, .access = S_ASR, .name = "STCC"},
@@ -117,7 +113,6 @@ static inline cap_register_t *get_scr(CPUArchState *env, uint32_t index)
     case CheriSCR_PCC: return &env->PCC;
     case CheriSCR_DDC: return &env->DDC;
 
-    case CheriSCR_UScratchC: return &env->UScratchC;
     case CheriSCR_UEPCC: return &env->UEPCC;
 
     case CheriSCR_STCC: return &env->STCC;
