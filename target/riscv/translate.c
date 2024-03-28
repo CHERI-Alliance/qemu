6b5ea59f475f1b05ca2486c71cbbe0c824197c8c
Paul Buxton <paul.buxton@codasip.com>
2024-03-28 09:23:43 +0000

    Replace single line comments with block comment

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -222,12 +222,14 @@ static inline void _gen_get_gpr(TCGv t, int reg_num)
 }
 
 #ifdef CONFIG_RVFI_DII
-//#define gen_get_gpr(t, reg_num, field_prefix)                                  \
-//    do {                                                                       \
-//        _gen_get_gpr(t, reg_num);                                              \
-//        gen_rvfi_dii_set_field(field_prefix##_data, t);                        \
-//        gen_rvfi_dii_set_field_const(field_prefix##_addr, reg_num);            \
-//    } while (0)
+/*
+#define gen_get_gpr(t, reg_num, field_prefix)                                  \
+   do {                                                                       \
+       _gen_get_gpr(t, reg_num);                                              \
+       gen_rvfi_dii_set_field(field_prefix##_data, t);                        \
+       gen_rvfi_dii_set_field_const(field_prefix##_addr, reg_num);            \
+   } while (0)
+*/
 #else
 // #define gen_get_gpr(t, reg_num, field) _gen_get_gpr(t, reg_num)
 #endif
