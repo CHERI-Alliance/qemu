fe454c25079208a688019ad48716b00e3e1377de
Paul Buxton <paul.buxton@codasip.com>
2024-04-03 15:31:02 +0100

    Add #defines for the CSR registers

--- target/riscv/cpu_bits.h
+++ target/riscv/cpu_bits.h
@@ -150,6 +150,7 @@
 #define CSR_MTVEC           0x305
 #define CSR_MCOUNTEREN      0x306
 
+
 /* 32-bit only */
 #define CSR_MSTATUSH        0x310
 
@@ -165,6 +166,16 @@
 #define CSR_MCAUSE          0x342
 #define CSR_MTVAL           0x343
 #define CSR_MIP             0x344
+
+
+#ifdef TARGET_CHERI
+/* Machine trap setup,scratch and handling capabilities*/
+#define CSR_MTVECC          0x765
+#define CSR_MSCRATCHC       0x760
+#define CSR_MEPCC           0x761
+#define CSR_MTDC            0x74c
+#endif
+
 
 /* Legacy Machine Trap Handling (priv v1.9.1) */
 #define CSR_MBADADDR        0x343
@@ -183,6 +194,14 @@
 #define CSR_SCAUSE          0x142
 #define CSR_STVAL           0x143
 #define CSR_SIP             0x144
+
+#ifdef TARGET_CHERI
+/* Supervisor trap setup, scratch and handling */
+#define CSR_STVECC          0x505
+#define CSR_SSCRATCHC       0x540
+#define CSR_SEPCC           0x541
+#define CSR_STDC            0x163
+#endif
 
 /* Legacy Supervisor Trap Handling (priv v1.9.1) */
 #define CSR_SBADADDR        0x143
@@ -264,6 +283,20 @@
 #define CSR_DCSR            0x7b0
 #define CSR_DPC             0x7b1
 #define CSR_DSCRATCH        0x7b2
+
+#ifdef TARGET_CHERI
+/* CHERI Debug mode capabilities */
+#define CSR_DPCC            0x7b9
+#define CSR_DSCRATCH0C      0x7ba
+#define CSR_DSCRATCH1C      0x7bb
+#define CSR_DDDC            0x7bc
+
+/* CHERI default and infinit capabiltiies */
+#define CSR_DDC             0x416
+#define CSR_DINFC           0x7bd
+
+#endif
+
 
 /* Performance Counters */
 #define CSR_MHPMCOUNTER3    0xb03
