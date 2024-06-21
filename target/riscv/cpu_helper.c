CHERI-Alliance/qemu

commit 2cc3ba325a485e6f5545fdcc27f2c552c736211b

riscv: fix pc / pcc update for trap handling
The code that raises a trap must update pc or pcc to jump into the trap
handler. If cheri is enabled at compile time, pcc is updated with the
trap handler's address. On systems without cheri support, pc is updated.

This does not cover the case where cheri is enabled but the system is in
integer pointer mode when the trap is raised. In this case, pc must be
updated.

Distinguish between capability pointer mode and integer pointer mode on
systems with cheri support.

Refactor the code to summarize common statements and comments.

target/riscv/cpu_helper.c
@@ -1229,23 +1229,31 @@ bool riscv_cpu_tlb_fill(CPUState *cs, vaddr address, int size,
 #endif
 }
 
-#ifdef TARGET_CHERI
-/* TODO(am2419): do we log PCC as a changed register? */
-#define riscv_update_pc_for_exc_handler(env, src_cap, new_pc)           \
-    do {                                                                \
-        cheri_update_pcc_for_exc_handler(&env->PCC, src_cap, new_pc);   \
-        qemu_log_instr_dbg_cap(env, "PCC", &env->PCC);                  \
-    } while (false)
-#else
+#define __UPDATE_PC { \
+   riscv_update_pc(env, new_pc, /*can_be_unrepresentable=*/ true); \
+   qemu_log_instr_dbg_reg(env, "pc", new_pc); \
+}
+
 /*
  * TODO(am2419): We don't have a register ID for pc, move to a separate
  * logging helper that maps hwreg id to names for extra registers.
  * TODO(am2419): do we log PCC as a changed register?
  */
+#ifdef TARGET_CHERI
+#define riscv_update_pc_for_exc_handler(env, src_cap, new_pc) \
+    do { \
+        if (cheri_in_capmode(env)) { \
+            cheri_update_pcc_for_exc_handler(&env->PCC, src_cap, new_pc); \
+            qemu_log_instr_dbg_cap(env, "PCC", &env->PCC); \
+        } \
+        else { \
+          __UPDATE_PC \
+        } \
+    } while (false)
+#else
 #define riscv_update_pc_for_exc_handler(env, src_cap, new_pc)           \
     do {                                                                \
-        riscv_update_pc(env, new_pc, /*can_be_unrepresentable=*/true);  \
-        qemu_log_instr_dbg_reg(env, "pc", new_pc);                      \
+        __UPDATE_PC                                                     \
     } while (false)
 #endif /* TARGET_CHERI */
 

commit 64c2641eeb44052646f82126801d348b4cefc2dc

cheri: riscv: use correct mode for exception handlers
Use the correct (capability or integer pointer) mode when we jump to an
exception handler.

When an exception is raised, the code checks the M bit in PCC to
determine the current mode. To jump into the exception handler, it
installs either the address in xTVEC or the capability in xTVECC as the
new PCC.

This does not work if the exception was caused by an invalid PCC. In this
case, we must not use this invalid PCC for setting up the exception
handler.

Rewrite the mode check to use the xTVECC CSR. If xTVECC contains a valid
capability that grants execute permission, we can safely assume that the
exception handler should run in capability pointer mode.

Fixes: 2cc3ba325a4 ("riscv: fix pc / pcc update for trap handling")

target/riscv/cpu_helper.c
@@ -1242,7 +1242,14 @@ bool riscv_cpu_tlb_fill(CPUState *cs, vaddr address, int size,
 #ifdef TARGET_CHERI
 #define riscv_update_pc_for_exc_handler(env, src_cap, new_pc) \
     do { \
-        if (cheri_in_capmode(env)) { \
+        /* 
+         * Attention: Do not call cheri_in_capmode(env) here. This checks
+         * the current PCC. Its content is not reliable at this point, the
+         * exception might have been caused by an invalid PCC.
+         * Instead, we assume that if xTVECC contains a valid capability,
+         * the exception handler is meant to run in capability pointer mode.
+         */ \
+        if (src_cap && cap_has_perms(src_cap, CAP_PERM_EXECUTE)) { \
             cheri_update_pcc_for_exc_handler(&env->PCC, src_cap, new_pc); \
             qemu_log_instr_dbg_cap(env, "PCC", &env->PCC); \
         } \