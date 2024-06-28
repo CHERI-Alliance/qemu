e79fb8d4673844101e462dc4b9e7b78655ee15dd
Paul Buxton <paul.buxton@codasip.com>
2024-06-28 16:59:34 +0200

    RVY: Add the two-bit PTE scheme for version 0.9.3 of the spec
    Disable use of CHERI PTE flags defined in the CHERI v9 specification
    which are not part of the RISC-V CHERI specification, and the code that
    used them. Update the definition/handling of the retained PTE flags to
    match the specification.
    
    0.9.3 specification adds the requirement to return a code for both
    CHERI_PTE exception as well as a regular pagefault. We could add this
    case to the get_phys_addr which is called during the tlb_fill.
    We only need to add the handling here because of the way the tlb entry
    is handled. The TLB maintains read and write address separately, which
    are setup the first time the TLB entry is used (and valid) for that
    access type. This means that a tlb fill is the only time that we can get
    both a CHERI pte exception as well as a regular page fault, as when we
    have the CHERI exception, the tlbentry has either been validated for the
    access type (in which case it is just a cheri exception), or has not
    been validated and thus requires a tlb fill.
    
    While porting this change to the latest version of QEMU the support for
    detecting both normal page faults and CHERI page faults was dropped and
    we just report the first detected fault. This behaviour no longer exists
    in the 0.9.6 spec so carrying the rather large diff was awkward.

--- target/riscv/monitor.c
+++ target/riscv/monitor.c
@@ -66,8 +66,10 @@ static void print_pte(Monitor *mon, int va_bits, target_ulong vaddr,
 
     monitor_printf(mon, TARGET_FMT_lx " " TARGET_FMT_plx " " TARGET_FMT_lx
                    " %c%c%c%c%c%c%c"
-#if defined(TARGET_CHERI) && !defined(TARGET_RISCV32)
+#if defined(TARGET_CHERI_RISCV_V9) && !defined(TARGET_RISCV32)
                    "%c%c%c%c%c"
+#elif defined(TARGET_CHERI_RISCV_STD_093) && !defined(TARGET_RISCV32)
+                   "%c%c"
 #endif
                    "\n",
                    addr_canonical(va_bits, vaddr),
@@ -82,9 +84,11 @@ static void print_pte(Monitor *mon, int va_bits, target_ulong vaddr,
 #if defined(TARGET_CHERI) && !defined(TARGET_RISCV32)
                    ,
                    attr & PTE_CRG ? 'G' : '-',
+#if defined(TARGET_CHERI_RISCV_V9)
                    attr & PTE_CRM ? 'M' : '-',
                    attr & PTE_CD  ? 'D' : '-',
                    attr & PTE_CR  ? 'R' : '-',
+#endif
                    attr & PTE_CW  ? 'W' : '-'
 #endif
         );
@@ -115,8 +119,10 @@ static void walk_pte(Monitor *mon, hwaddr base, target_ulong start,
         cpu_physical_memory_read(pte_addr, &pte, ptesize);
 
         paddr = (hwaddr)(pte >> PTE_PPN_SHIFT) << PGSHIFT;
-#if defined(TARGET_CHERI) && !defined(TARGET_RISCV32)
+#if defined(TARGET_CHERI_RISCV_V9) && !defined(TARGET_RISCV32)
         attr = pte & (PTE_CR | PTE_CW | PTE_CD | PTE_CRM | PTE_CRG | 0xff);
+#elif defined(TARGET_CHERI_RISCV_STD_093) && !defined(TARGET_RISCV32)
+        attr = pte & (PTE_CW | PTE_CRG | 0xff);
 #else
         attr = pte & 0xff;
 #endif
