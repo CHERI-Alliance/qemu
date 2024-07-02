4a0805138fded00832e9dc92c9ebee9f526c2951
Martin Kaiser <martin.kaiser@codasip.com>
2024-07-02 17:05:32 +0200

    cheri: riscv: remove leftovers of csetboundsimm
    Remove the last bits o code for the csetboundsimm instruction. It seems
    that we forgot them when we changed from csetboundsimm to scbndsi.

--- disas/riscv.c
+++ disas/riscv.c
@@ -497,7 +497,6 @@ typedef enum {
     rv_op_csw,
     rv_op_csd,
 
-    rv_op_csetboundsimm,
     rv_op_modesw,
     // Two operand
     rv_op_crrl,
@@ -1204,7 +1203,6 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_clc] = { "clc", rv_codec_i, rv_fmt_cd_offset_cs1, NULL, 0, 0, 0 },
     [rv_op_sc] = { "sc", rv_codec_s, rv_fmt_cs2_offset_rs1, NULL, 0, 0, 0 },
     [rv_op_csc] = { "csc", rv_codec_s, rv_fmt_cs2_offset_cs1, NULL, 0, 0, 0 },
-    [rv_op_csetboundsimm] = { "csetbounds", rv_codec_i, rv_fmt_cd_cs1_imm, NULL, 0, 0, 0 },
     [rv_op_modesw] = { "modesw", rv_codec_none, rv_fmt_none, NULL, 0, 0, 0 },
     // Two operand
     [rv_op_crrl] = { "crrl", rv_codec_r, rv_fmt_rd_rs1, NULL, 0, 0, 0 },
@@ -2111,9 +2109,6 @@ static void decode_inst_opcode(rv_decode *dec, rv_isa isa, int flags)
                 switch (((inst >> 12) & 0b111)) {
                 case 0:
                     op = decode_cheri_inst(inst);
-                    break;
-                case 2:
-                    op = rv_op_csetboundsimm;
                     break;
                 }
             }
