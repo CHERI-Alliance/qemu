63c37c8fd99f7e1566752acdde01578596140672
Paul Buxton <paul.buxton@codasip.com>
2024-07-16 15:24:48 +0100

    Replace the use of pred_capmode with pred_cre for cheri instructions.
    pred_capmode was not the correct predicate to use to disable cheri instructions. Instead it should be based just off of the cre.

--- target/riscv/translate.c
+++ target/riscv/translate.c
@@ -71,7 +71,8 @@ typedef struct DisasContext {
     bool hlsx;
 #ifdef TARGET_CHERI
     bool capmode;
-    bool hybrid; 
+    bool hybrid;
+    bool cre;
 #endif
     /* vector extension */
     bool vill;
@@ -625,6 +626,12 @@ static bool pred_hybrid(DisasContext *ctx)
 }
 #endif
 
+#ifdef TARGET_CHERI
+static bool pred_cre(DisasContext *ctx)
+{
+    return ctx->cre;
+}
+#endif
 
 /* Include the auto-generated decoder for 32 bit insn */
 #include "decode-insn32.c.inc"
@@ -949,6 +956,7 @@ static void riscv_tr_init_disas_context(DisasContextBase *dcbase, CPUState *cs)
 #ifdef TARGET_CHERI
     ctx->capmode = tb_in_capmode(ctx->base.tb);
     ctx->hybrid = riscv_feature(env, RISCV_FEATURE_CHERI_HYBRID);
+    ctx->cre = riscv_cpu_mode_cre(env);
 #endif
     ctx->priv_ver = env->priv_ver;
 #if !defined(CONFIG_USER_ONLY)
