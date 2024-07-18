b071707d3f90aa6f3286b80ada9deed8898fc4df
Paul Buxton <paul.buxton@codasip.com>
2024-07-18 15:49:51 +0100

    Add the c.lcsp Compressed capability instruction decoder.

--- disas/riscv.c
+++ disas/riscv.c
@@ -581,6 +581,7 @@ typedef enum {
     // CHERI compressed capmode load/stores
     rv_op_c_lc,
     rv_op_c_sc,
+    rv_op_c_lcsp,
 } rv_op;
 
 /* structures */
@@ -1328,6 +1329,8 @@ const rv_opcode_data opcode_data[] = {
                      0 },
     [rv_op_c_sc] = { "sc", rv_codec_cs_sq, rv_fmt_cs2_offset_cs1, NULL, 0, 0,
                      0 },
+    [rv_op_c_lcsp] = { "lc", rv_codec_ci_lqsp, rv_fmt_cd_offset_cs1, NULL, 0, 0,
+                     0 },
 
     // Three operand
     [rv_op_scbnds] = { "scbnds", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
@@ -1691,7 +1694,7 @@ static void decode_inst_opcode(rv_decode *dec, rv_isa isa, int flags)
             if (isa == rv128) {
                 op = rv_op_c_lqsp;
             } else {
-                op = rv_op_c_fldsp;
+                op = (flags & RISCV_DIS_FLAG_CAPMODE) ? rv_op_c_lcsp : rv_op_c_fldsp;
             }
             break;
         case 2: op = rv_op_c_lwsp; break;
