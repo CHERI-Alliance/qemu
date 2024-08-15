CHERI-Alliance/qemu

commit 397ed93aa2bb88ea205f3d0f4685d6c54786f2cc

Flush the TCG when logging mode changes.
If a code block has previously been translated prior to a logging level change and it is subsequently executed then the execution can occur with the wrong logging level resulting in confusing log output.
This partially reverts commit 9aa3707.

accel/tcg/log_instr.c
@@ -683,9 +683,9 @@ static void do_cpu_loglevel_switch(CPUState *cpu, run_on_cpu_data data)
     if (next_level == prev_level && prev_level_active == next_level_active)
         return;
 
-    /* Flushing all translations makes things incredibly slow. Instead,
-     * we put whether tracing is currently enabled into cflags */
-
+    /* A TCG Flush is required here else already translated code block
+    will not see the change in logging mode */
+    tb_flush(cpu);
     /* Emit start/stop events */
     if (prev_level_active) {
         if (cpulog->starting) {

commit 7bea2b5b21dbbabb41f2fb8a2928d96cb83edf49

log_instr: skip tb_flush before tcg was initialised
Commit 397ed93aa2b ("Flush the TCG when logging mode changes.") added a
tb_flush when the log level of instruction logging was changed.

The log level is processed when a sequence of guest instructions is
translated into a translation buffer. Dynamic updates of the log level
require a flush and a new translation.

It turned out that at qemu startup, tb_flush might be called before tcg is
initialized and a thread is created for the vcpu. The sequence is

qemu_log_instr_init calls
   do_cpu_loglevel_switch
      tb_flush
         async_safe_run_on_cpu
            queue_work_on_cpu (cpu=<optimized out>, wi=<optimized out>) at ../cpus-common.c:131
               qemu_cpu_kick (cpu=0x55555667e020) at ../softmmu/cpus.c:451
                  qemu_cond_broadcast (cond=0x0) at ../util/qemu-thread-posix.c:162

This happens only if -d instr is set on the qemu commandline. Otherwise,
the first call to do_cpu_loglevel_switch will not switch the log level.

This sequence causes a crash as cpu->halt_cond is NULL in
qemu_cond_broadcast. cpu->halt_cond is initialised by
rr_start_vcpu_thread, which also initialises cpu->thread.

Work around the crash and skip the tb_flush call if cpu->thread is NULL.

Fixes: 397ed93aa2b ("Flush the TCG when logging mode changes."

accel/tcg/log_instr.c
@@ -683,9 +683,12 @@ static void do_cpu_loglevel_switch(CPUState *cpu, run_on_cpu_data data)
     if (next_level == prev_level && prev_level_active == next_level_active)
         return;
 
-    /* A TCG Flush is required here else already translated code block
-    will not see the change in logging mode */
-    tb_flush(cpu);
+    if (cpu->thread) {
+        /* A TCG Flush is required here else already translated code block
+           will not see the change in logging mode */
+        tb_flush(cpu);
+    }
+
     /* Emit start/stop events */
     if (prev_level_active) {
         if (cpulog->starting) {