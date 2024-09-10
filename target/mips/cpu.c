CHERI-Alliance/qemu

commit 8c8daab727001f2031201cacb0f1ba910f138774

Implment register logging interface.
General purpose registers are logged during translation. We ditch the
previous approach of scanning the register set for changed registers as
this causes confusion in traces where there might be instructions that
happen to write the same existing value in a register, resulting in the
update going detected and potentially picking up the value of an unrelated
register that changed as a side-effect of that instruction.
With the current approach we pick up ALL intendend register writes.

Port the MIPS implementation to the new logging interface. This logs
all GPR, CP0 and partially support capabilty register logging.
Tentatively port most of the mips implementation to the new GPR logging
interface.
Cop0 registers are reported as generic integer register changes in the
generic instruction logging interface.

target/mips/cpu.c
@@ -32,6 +32,153 @@
 #include "exec/exec-all.h"
 #include "qemu/error-report.h"
 
+#ifndef TARGET_MIPS64
+const char mips_gp_regnames[32][5] = {
+    {"zero"}, {"at"},   {"v0"},   {"v1"},   {"a0"},   {"a1"},   {"a2"},   {"a3"},
+    {"t0"},   {"t1"},   {"t2"},   {"t3"},   {"t4"},   {"t5"},   {"t6"},   {"t7"},
+    {"s0"},   {"s1"},   {"s2"},   {"s3"},   {"s4"},   {"s5"},   {"s6"},   {"s7"},
+    {"t8"},   {"t9"},   {"k0"},   {"k1"},   {"gp"},   {"sp"},   {"s8"},   {"ra"
+};
+#else
+// Use n64 register names
+const char mips_gp_regnames[32][5] = {
+    {"zero"}, {"at"},   {"v0"},   {"v1"},   {"a0"},   {"a1"},   {"a2"},   {"a3"},
+    {"a4"},   {"a5"},   {"a6"},   {"a7"},   {"t0"},   {"t1"},   {"t2"},   {"t3"},
+    {"s0"},   {"s1"},   {"s2"},   {"s3"},   {"s4"},   {"s5"},   {"s6"},   {"s7"},
+    {"t8"},   {"t9"},   {"k0"},   {"k1"},   {"gp"},   {"sp"},   {"s8"},   {"ra"},
+};
+#endif
+
+const char mips_regnames_HI[4][4] = {
+    {"HI0"}, {"HI1"}, {"HI2"}, {"HI3"},
+};
+
+const char mips_regnames_LO[4][4] = {
+    {"LO0"}, {"LO1"}, {"LO2"}, {"LO3"},
+};
+
+const char mips_fregnames[32][4] = {
+    {"f0"},  {"f1"},  {"f2"},  {"f3"},  {"f4"},  {"f5"},  {"f6"},  {"f7"},
+    {"f8"},  {"f9"},  {"f10"}, {"f11"}, {"f12"}, {"f13"}, {"f14"}, {"f15"},
+    {"f16"}, {"f17"}, {"f18"}, {"f19"}, {"f20"}, {"f21"}, {"f22"}, {"f23"},
+    {"f24"}, {"f25"}, {"f26"}, {"f27"}, {"f28"}, {"f29"}, {"f30"}, {"f31"},
+};
+
+const char mips_msaregnames[64][7] = {
+    {"w0.d0"},  {"w0.d1"},  {"w1.d0"},  {"w1.d1"},
+    {"w2.d0"},  {"w2.d1"},  {"w3.d0"},  {"w3.d1"},
+    {"w4.d0"},  {"w4.d1"},  {"w5.d0"},  {"w5.d1"},
+    {"w6.d0"},  {"w6.d1"},  {"w7.d0"},  {"w7.d1"},
+    {"w8.d0"},  {"w8.d1"},  {"w9.d0"},  {"w9.d1"},
+    {"w10.d0"}, {"w10.d1"}, {"w11.d0"}, {"w11.d1"},
+    {"w12.d0"}, {"w12.d1"}, {"w13.d0"}, {"w13.d1"},
+    {"w14.d0"}, {"w14.d1"}, {"w15.d0"}, {"w15.d1"},
+    {"w16.d0"}, {"w16.d1"}, {"w17.d0"}, {"w17.d1"},
+    {"w18.d0"}, {"w18.d1"}, {"w19.d0"}, {"w19.d1"},
+    {"w20.d0"}, {"w20.d1"}, {"w21.d0"}, {"w21.d1"},
+    {"w22.d0"}, {"w22.d1"}, {"w23.d0"}, {"w23.d1"},
+    {"w24.d0"}, {"w24.d1"}, {"w25.d0"}, {"w25.d1"},
+    {"w26.d0"}, {"w26.d1"}, {"w27.d0"}, {"w27.d1"},
+    {"w28.d0"}, {"w28.d1"}, {"w29.d0"}, {"w29.d1"},
+    {"w30.d0"}, {"w30.d1"}, {"w31.d0"}, {"w31.d1"},
+};
+
+#if !defined(TARGET_MIPS64)
+const char * const mips_mxuregnames[16][7] = {
+    {"XR1"},  {"XR2"},  {"XR3"},  {"XR4"},  {"XR5"},  {"XR6"},  {"XR7"},  {"XR8"},
+    {"XR9"},  {"XR10"}, {"XR11"}, {"XR12"}, {"XR13"}, {"XR14"}, {"XR15"}, {"MXU_CR"},
+};
+#endif
+
+/*
+ * Names of coprocessor 0 registers.
+ */
+const char mips_cop0_regnames[32*8][32] = {
+/*0*/   {"Index"},        {"MVPControl"},   {"MVPConf0"},     {"MVPConf1"},
+        {0},              {0},              {0},              {0},
+/*1*/   {"Random"},       {"VPEControl"},   {"VPEConf0"},     {"VPEConf1"},
+        {"YQMask"},       {"VPESchedule"},  {"VPEScheFBack"}, {"VPEOpt"},
+/*2*/   {"EntryLo0"},     {"TCStatus"},     {"TCBind"},       {"TCRestart"},
+        {"TCHalt"},       {"TCContext"},    {"TCSchedule"},   {"TCScheFBack"},
+/*3*/   {"EntryLo1"},     {0},              {0},              {0},
+        {0},              {0},              {0},              {"TCOpt"},
+/*4*/   {"Context"},      {"ContextConfig"}, {"UserLocal"},    {"XContextConfig"},
+        {0},              {0},              {0},              {0},
+/*5*/   {"PageMask"},     {"PageGrain"},    {"SegCtl0"},      {"SegCtl1"},
+        {"SegCtl2"},      {0},              {0},              {0},
+/*6*/   {"Wired"},        {"SRSConf0"},     {"SRSConf1"},     {"SRSConf2"},
+        {"SRSConf3"},     {"SRSConf4"},     {0},              {0},
+/*7*/   {"HWREna"},       {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*8*/   {"BadVAddr"},     {"BadInstr"},     {"BadInstrP"},    {0},
+        {0},              {0},              {0},              {0},
+/*9*/   {"Count"},        {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*10*/  {"EntryHi"},      {0},              {0},              {0},
+        {0},              {"MSAAccess"},    {"MSASave"},      {"MSARequest"},
+/*11*/  {"Compare"},      {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*12*/  {"Status"},       {"IntCtl"},       {"SRSCtl"},       {"SRSMap"},
+        {"ViewIPL"},      {"SRSMap2"},      {0},              {0},
+/*13*/  {"Cause"},        {0},              {0},              {0},
+        {"ViewRIPL"},     {"NestedExc"},    {0},              {0},
+/*14*/  {"EPC"},          {0},              {"NestedEPC"},    {0},
+        {0},              {0},              {0},              {0},
+/*15*/  {"PRId"},         {"EBase"},        {"CDMMBase"},     {"CMGCRBase"},
+        {0},              {0},              {0},              {0},
+/*16*/  {"Config"},       {"Config1"},      {"Config2"},      {"Config3"},
+        {"Config4"},      {"Config5"},      {"Config6"},      {"Config7"},
+/*17*/  {"LLAddr"},       {"internal_lladdr (virtual)"}, {"internal_llval"}, {"internal_linkedflag"},
+        {0},              {0},              {0},              {0},
+/*18*/  {"WatchLo"},      {"WatchLo1"},     {"WatchLo2"},     {"WatchLo3"},
+        {"WatchLo4"},     {"WatchLo5"},     {"WatchLo6"},     {"WatchLo7"},
+/*19*/  {"WatchHi"},      {"WatchHi1"},     {"WatchHi2"},     {"WatchHi3"},
+        {"WatchHi4"},     {"WatchHi5"},     {"WatchHi6"},     {"WatchHi7"},
+/*20*/  {"XContext"},     {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*21*/  {0},              {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*22*/  {0},              {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*23*/  {"Debug"},        {"TraceControl"}, {"TraceControl2"}, {"UserTraceData"},
+        {"TraceIBPC"},    {"TraceDBPC"},    {"Debug2"},       {0},
+/*24*/  {"DEPC"},         {0},              {"TraceControl3"}, {"UserTraceData2"},
+        {0},              {0},              {0},              {0},
+/*25*/  {"PerfCnt"},      {"PerfCnt1"},     {"PerfCnt2"},     {"PerfCnt3"},
+        {"PerfCnt4"},     {"PerfCnt5"},     {"PerfCnt6"},     {"PerfCnt7"},
+/*26*/  {"ErrCtl"},       {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*27*/  {"CacheErr"},     {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*28*/  {"ITagLo"},       {"IDataLo"},      {"DTagLo"},       {"DDataLo"},
+        {"L23TagLo"},     {"L23DataLo"},    {0},              {0},
+/*29*/  {"ITagHi"},       {"IDataHi"},      {"DTagHi"},       {0},
+        {0},              {"L23DataHi"},    {0},              {0},
+/*30*/  {"ErrorEPC"},     {0},              {0},              {0},
+        {0},              {0},              {0},              {0},
+/*31*/  {"DESAVE"},       {0},              {"KScratch1"},    {"KScratch2"},
+        {"KScratch3"},    {"KScratch4"},    {"KScratch5"},    {"KScratch6"},
+};
+
+#ifdef TARGET_CHERI
+const char cheri_gp_regnames[32][4] = {
+    {"C00"}, {"C01"}, {"C02"}, {"C03"}, {"C04"}, {"C05"}, {"C06"}, {"C07"},
+    {"C08"}, {"C09"}, {"C10"}, {"C11"}, {"C12"}, {"C13"}, {"C14"}, {"C15"},
+    {"C16"}, {"C17"}, {"C18"}, {"C19"}, {"C20"}, {"C21"}, {"C22"}, {"C23"},
+    {"C24"}, {"C25"}, {"C26"}, {"C27"}, {"C28"}, {"C29"}, {"C30"}, {"C31"},
+};
+
+const char mips_cheri_hw_regnames[32][10] = {
+    {"DDC"},       {"UserTLS"}, {0},      {0},
+    {0},           {0},         {0},      {0},
+    {"PrivTLS"},   {0},         {0},      {0},
+    {0},           {0},         {0},      {0},
+    {0},           {0},         {0},      {0},
+    {0},           {0},         {"KR1C"}, {"KR2C"},
+    {0},           {0},         {0},      {0},
+    {"ErrorEPCC"}, {"KCC"},     {"KDC"},  {"EPCC"},
+};
+#endif
 
 static void mips_cpu_set_pc(CPUState *cs, vaddr value)
 {

accel/tcg/log_instr.c
@@ -66,7 +66,10 @@
 #endif
 
 struct log_reginfo {
-    bool is_capability;
+    uint16_t flags;
+#define LRI_CAP_REG    1
+#define LRI_HOLDS_CAP  2
+
     const char *name;
     union {
         target_ulong gpr;
@@ -76,6 +79,9 @@ struct log_reginfo {
     };
 };
 typedef struct log_reginfo log_reginfo_t;
+
+#define reginfo_is_cap(ri) (ri->flags & LRI_CAP_REG)
+#define reginfo_has_cap(ri) (reginfo_is_cap(ri) && (ri->flags & LRI_HOLDS_CAP))
 
 extern int cl_default_trace_format;
 
@@ -94,6 +100,8 @@ static cpu_log_instr_info_t *target_cpu_get_log(CPUArchState *env)
  */
 static void emit_text_trace(CPUArchState *env, cpu_log_instr_info_t *log)
 {
+    int i;
+
     /* Dump main instruction log */
 #if defined(TARGET_RISCV) && defined(CONFIG_RVFI_DII)
     /*
@@ -110,8 +118,34 @@ static void emit_text_trace(CPUArchState *env, cpu_log_instr_info_t *log)
         log_target_disas(env_cpu(env), log->pc, /*only one*/-1);
     }
 
-    /* Dump register changes and side-effects */
+    /* Dump memory access */
 
+    /* Dump register changes and side-effects */
+    for (i = 0; i < log->regs->len; i++) {
+        log_reginfo_t *rinfo = &g_array_index(log->regs, log_reginfo_t, i);
+
+#ifndef TARGET_CHERI
+        log_assert(!reginfo_is_cap(rinfo) && "Register marked as capability "
+                   "register whitout CHERI support");
+#else
+        if (reginfo_is_cap(rinfo)) {
+            if (reginfo_has_cap(rinfo))
+                qemu_log("newapi    Write %s|" PRINT_CAP_FMTSTR_L1 "\n"
+                         "             |" PRINT_CAP_FMTSTR_L2 "\n",
+                         rinfo->name,
+                         PRINT_CAP_ARGS_L1(&rinfo->cap),
+                         PRINT_CAP_ARGS_L2(&rinfo->cap));
+            else
+                qemu_log("newapi  %s <- " TARGET_FMT_lx
+                         " (setting integer value)\n",
+                         rinfo->name, rinfo->gpr);
+        } else
+#endif
+        {
+            qemu_log("newapi    Write %s = " TARGET_FMT_lx "\n", rinfo->name,
+                     rinfo->gpr);
+        }
+    }
 
     /* Dump extra logged messages, if any */
     if (log->txt_buffer->len > 0)
@@ -148,10 +182,10 @@ static void emit_cvtrace(cpu_log_instr_info_t *log)
 static void emit_log_start(CPUArchState *env, target_ulong pc)
 {
     if (qemu_loglevel_mask(CPU_LOG_INSTR)) {
-        qemu_log("Requested instruction logging @ " TARGET_FMT_plx
+        qemu_log("Requested instruction logging @ " TARGET_FMT_lx
                  " ASID %u\n", pc, cheri_get_asid(env));
-                    qemu_log("User-mode only tracing enabled at " TARGET_FMT_lx
-                     ", ASID %u\n", pc, cheri_get_asid(env));
+        qemu_log("User-mode only tracing enabled at " TARGET_FMT_lx
+                 ", ASID %u\n", pc, cheri_get_asid(env));
     } else if (qemu_loglevel_mask(CPU_LOG_CVTRACE)) {
         // TODO(am2419) Emit an event for instruction logging start
     }
@@ -164,8 +198,8 @@ static void emit_log_stop(CPUArchState *env, target_ulong pc)
             qemu_log("User-mode only tracing disabled at " TARGET_FMT_lx
                      ", ASID %u\n", pc, cheri_get_asid(env));
         else
-            qemu_log("Disabled instruction logging @ " TARGET_FMT_plx " ASID %u\n",
-                     pc, cheri_get_asid(env));
+            qemu_log("Disabled instruction logging @ " TARGET_FMT_lx
+                     " ASID %u\n", pc, cheri_get_asid(env));
     } else if (qemu_loglevel_mask(CPU_LOG_CVTRACE)) {
         // TODO(am2419) Emit an event for instruction logging stop
     }
@@ -376,26 +410,49 @@ out:
     log->force_drop = false;
 }
 
-void _qemu_log_instr_reg(CPUArchState *env, const char *reg_name, target_ulong value)
+/*
+ * TODO(am2419): how do we deal with orderding in case multiple registers are updated?
+ * Maybe add a log_instr_reg_extra to distinguish from primary side-effect and other
+ * side effects? (e.g. primary side effect is to update destination register, secondary
+ * might be to clear status flag registers).
+ * This is however an half-solution to the limitations imposed by the binary trace format,
+ * we may just discard the assumption that there is any ordering at all and all side-effects
+ * are granted equal rights under the constitution.
+ */
+void _qemu_log_instr_reg(CPUArchState *env, const char *reg_name,
+                         target_ulong value)
 {
     cpu_log_instr_info_t *log = target_cpu_get_log(env);
     log_reginfo_t r;
 
-    r.is_capability = false;
+    r.flags = 0;
     r.name = reg_name;
     r.gpr = value;
     g_array_append_val(log->regs, r);
 }
 
 #ifdef TARGET_CHERI
-void _qemu_log_instr_cap(CPUArchState *env, const char *reg_name, cap_register_t *cr)
+void _qemu_log_instr_cap(CPUArchState *env, const char *reg_name,
+                         const cap_register_t *cr)
 {
     cpu_log_instr_info_t *log = target_cpu_get_log(env);
     log_reginfo_t r;
 
-    r.is_capability = true;
+    r.flags = LRI_CAP_REG | LRI_HOLDS_CAP;
     r.name = reg_name;
     r.cap = *cr;
+    g_array_append_val(log->regs, r);
+}
+
+void _qemu_log_instr_cap_int(CPUArchState *env, const char *reg_name,
+                             target_ulong value)
+{
+    cpu_log_instr_info_t *log = target_cpu_get_log(env);
+    log_reginfo_t r;
+
+    r.flags = LRI_CAP_REG;
+    r.name = reg_name;
+    r.gpr = value;
     g_array_append_val(log->regs, r);
 }
 #endif

include/exec/log_instr.h
@@ -46,6 +46,14 @@
  * The following hooks must be implemented:
  * - log_instr_changed_state
  * - tb_in_user_mode
+ *
+ * Each target should implement their own register update logging helpers that
+ * call into qemu_log_instr_gpr(), qemu_log_instr_cap() and similar interface
+ *  functions.
+ * Note that the target is responsible for producing a register name to use.
+ * It is recommended to use the helper_<target>_log_instr_{gpr,cap..}() naming
+ * convention to distinguish target-specific and generic qemu_log_instr
+ * interfaces.
  */
 
 #ifdef CONFIG_CHERI_LOG_INSTR
@@ -77,6 +85,8 @@ void qemu_log_instr_start(CPUArchState *env, uint32_t mode, target_ulong pc);
  */
 void qemu_log_instr_stop(CPUArchState *env, uint32_t mode, target_ulong pc);
 
+// TODO(am2419): the maybe part is redundant, we only call these from helpers or
+// translator code which checks for logging enabled anyway.
 #define qemu_maybe_log_instr(op, ...) do {                      \
         if (unlikely(qemu_loglevel_mask(INSTR_LOG_MASK)))       \
             op(__VA_ARGS__);                                    \
@@ -91,8 +101,11 @@ void qemu_log_instr_stop(CPUArchState *env, uint32_t mode, target_ulong pc);
 #ifdef TARGET_CHERI
 #define qemu_log_instr_cap(...)                                 \
     qemu_maybe_log_instr(_qemu_log_instr_cap, __VA_ARGS__)
+#define qemu_log_instr_cap_int(...)                             \
+    qemu_maybe_log_instr(_qemu_log_instr_cap_int, __VA_ARGS__)
 #else
 #define qemu_log_instr_cap(...)
+#define qemu_log_instr_cap_int(...)
 #endif
 
 #define qemu_log_instr_mem(...)                                 \
@@ -149,7 +162,13 @@ void _qemu_log_instr_reg(CPUArchState *env, const char *reg_name,
  * Log changed capability register.
  */
 void _qemu_log_instr_cap(CPUArchState *env, const char *reg_name,
-                         cap_register_t *cr);
+                         const cap_register_t *cr);
+
+/*
+ * Log changed capability register with integer value.
+ */
+void _qemu_log_instr_cap_int(CPUArchState *env, const char *reg_name,
+                             target_ulong value);
 #endif
 
 /*

target/cheri-common/cheri-lazy-capregs.h
@@ -43,6 +43,7 @@
 #include "cpu.h"
 #include "tcg/tcg.h"
 #include "qemu/log.h"
+#include "exec/log_instr.h"
 
 static inline GPCapRegs *cheri_get_gpcrs(CPUArchState *env);
 
@@ -180,25 +181,33 @@ get_capreg_0_is_ddc(CPUArchState *env, unsigned regnum)
 
 
 #ifdef CONFIG_CHERI_LOG_INSTR
-extern const char * const cheri_gp_regnames[];
-#define log_changed_capreg(env, name, newval) \
-    qemu_log_mask_and_addr(CPU_LOG_INSTR, cpu_get_recent_pc(env), \
-                           "  %s <- " PRINT_CAP_FMTSTR "\n", name, \
-                           PRINT_CAP_ARGS(newval))
-static inline void log_changed_capreg_int(CPUArchState *env, const char *name,
-                                          target_ulong newval)
-{
-    if (unlikely(qemu_loglevel_mask(CPU_LOG_INSTR)) &&
-        qemu_log_in_addr_range(cpu_get_recent_pc(env))) {
-        qemu_log("  %s <- " TARGET_FMT_lx " (setting integer value)\n", name,
-                 newval);
-    }
-}
+
+/*
+ * Log instruction update to the given capability register.
+ */
+#define log_changed_capreg(env, name, newval) do {                      \
+        if (unlikely(qemu_log_instr_enabled(env_cpu(env)))) {           \
+            qemu_log_instr_cap(env, name, newval);                      \
+        }                                                               \
+    } while (0)
+
+/*
+ * Log instruction update to the given capability register with
+ * integer value.
+ */
+#define log_changed_capreg_int(env, name, newval) do {          \
+        if (unlikely(qemu_log_instr_enabled(env_cpu(env)))) {   \
+            qemu_log_instr_cap_int(env, name, newval);          \
+        }                                                       \
+    } while (0)
+
 #else
 #define log_changed_capreg(env, name, newval) ((void)0)
 #define log_changed_capreg_int(env, name, newval) ((void)0)
 #endif
-#define log_changed_gp_capreg(env, regnum, newval)                             \
+
+/* Note: cheri_gp_regnames should be declared in cpu.h or cheri-archspecific.h */
+#define log_changed_gp_capreg(env, regnum, newval)              \
     log_changed_capreg(env, cheri_gp_regnames[regnum], newval)
 
 static inline void rvfi_changed_capreg(CPUArchState *env, unsigned regnum,

target/mips/cpu.h
@@ -1278,6 +1278,19 @@ struct MIPSCPU {
     CPUMIPSState env;
 };
 
+/* Register names for logging output */
+extern const char mips_gp_regnames[32][5];
+extern const char mips_regnames_HI[4][4];
+extern const char mips_regnames_LO[4][4];
+extern const char mips_fregnames[32][4];
+extern const char mips_msaregnames[64][7];
+extern const char mips_mxuregnames[16][7];
+extern const char mips_cop0_regnames[32*8][32];
+#ifdef TARGET_CHERI
+/* Needed for cheri-common logging */
+extern const char cheri_gp_regnames[32][4];
+extern const char mips_cheri_hw_regnames[32][10];
+#endif
 
 void mips_cpu_list(void);
 

target/mips/helper.h
@@ -187,6 +187,8 @@ DEF_HELPER_1(mfc0_coreid, tl, env)
 
 #ifdef CONFIG_CHERI_LOG_INSTR
 /* Target dependant-helpers */
+DEF_HELPER_3(mips_log_instr_gpr, void, env, i32, tl)
+DEF_HELPER_4(mips_log_instr_cop0, void, env, i32, i32, tl)
 DEF_HELPER_2(mips_log_instr_changed_state, void, env, tl)
 /* Old stuff TODO(am2419): deprecate */
 DEF_HELPER_FLAGS_2(mips_cvtrace_log_instruction, TCG_CALL_NO_RWG, void, env, i64)

target/mips/op_helper_cheri.c
@@ -48,6 +48,7 @@
 
 #include "cheri_tagmem.h"
 #include "cheri-helper-utils.h"
+#include "cheri-lazy-capregs.h"
 
 #include "disas/disas.h"
 #include "disas/dis-asm.h"
@@ -96,11 +97,23 @@ is_cap_sealed(const cap_register_t *cp)
     return cap_is_sealed_with_type(cp) || cap_is_sealed_entry(cp);
 }
 
+// TODO(am2419): deprecated, remove
 void qemu_log_capreg(const cap_register_t *cr, const char* prefix, const char* name) {
     qemu_log("%s%s|" PRINT_CAP_FMTSTR_L1 "\n"
              "             |" PRINT_CAP_FMTSTR_L2 "\n",
              prefix, name, PRINT_CAP_ARGS_L1(cr), PRINT_CAP_ARGS_L2(cr));
 }
+
+#ifdef CONFIG_CHERI_LOG_INSTR
+#define log_instr_hwreg_update(env, name, newval) do {   \
+        if (unlikely(qemu_log_instr_enabled(env_cpu(env)))) {   \
+            qemu_log_instr_reg(env, name, newval);              \
+        }                                                       \
+    } while (0)
+
+#else
+#define log_instr_cop2_update(env, name, newval) ((void)0)
+#endif
 
 static inline int align_of(int size, uint64_t addr)
 {
@@ -131,8 +144,11 @@ static bool cap_exactly_equal(const cap_register_t *cbp, const cap_register_t *c
 
 static inline void update_ddc(CPUArchState *env, const cap_register_t* new_ddc) {
     if (!cap_exactly_equal(&env->active_tc.CHWR.DDC, new_ddc)) {
-        qemu_log_mask_and_addr(
-            CPU_LOG_INSTR | CPU_LOG_MMU, cpu_get_recent_pc(env),
+        if (qemu_log_instr_enabled(env_cpu(env)))
+            qemu_log_instr_extra(
+                env, "Flushing TCG TLB since $ddc is changing to "
+                PRINT_CAP_FMTSTR "\n", PRINT_CAP_ARGS(new_ddc));
+        qemu_log_mask_and_addr(CPU_LOG_MMU, cpu_get_recent_pc(env),
             "Flushing TCG TLB since $ddc is changing to " PRINT_CAP_FMTSTR "\n",
             PRINT_CAP_ARGS(new_ddc));
         // TODO: in the future we may want to move $ddc to the guest -> host addr
@@ -144,11 +160,15 @@ static inline void update_ddc(CPUArchState *env, const cap_register_t* new_ddc) 
         // XXX: tlb_flush(env_cpu(env));
         env->active_tc.CHWR.DDC = *new_ddc;
     } else {
-        qemu_log_mask_and_addr(
-            CPU_LOG_INSTR | CPU_LOG_MMU, cpu_get_recent_pc(env),
+        if (qemu_log_instr_enabled(env_cpu(env)))
+            qemu_log_instr_extra(
+                env, "Installing same $ddc again, not flushing TCG TLB: "
+                PRINT_CAP_FMTSTR "\n", PRINT_CAP_ARGS(new_ddc));
+        qemu_log_mask_and_addr(CPU_LOG_MMU, cpu_get_recent_pc(env),
             "Installing same $ddc again, not flushing TCG TLB: " PRINT_CAP_FMTSTR "\n",
             PRINT_CAP_ARGS(new_ddc));
     }
+    log_changed_capreg(env, mips_cheri_hw_regnames[0], new_ddc);
 }
 
 target_ulong CHERI_HELPER_IMPL(cbez(CPUArchState *env, uint32_t cb, uint32_t offset))
@@ -478,6 +498,8 @@ void CHERI_HELPER_IMPL(mtc0_epc(CPUArchState *env, target_ulong arg))
         raise_cheri_exception(env, CapEx_AccessSystemRegsViolation, CP2HWR_EPCC);
     }
     set_CP0_EPC(env, arg + cap_get_base(&env->active_tc.CHWR.EPCC));
+    log_instr_hwreg_update(env, mips_cop0_regnames[14],
+                           env->active_tc.CHWR.EPCC._cr_cursor);
 }
 
 void CHERI_HELPER_IMPL(mtc0_error_epc(CPUArchState *env, target_ulong arg))
@@ -490,11 +512,14 @@ void CHERI_HELPER_IMPL(mtc0_error_epc(CPUArchState *env, target_ulong arg))
         raise_cheri_exception(env, CapEx_AccessSystemRegsViolation, CP2HWR_ErrorEPCC);
     }
     set_CP0_ErrorEPC(env, arg + cap_get_base(&env->active_tc.CHWR.ErrorEPCC));
+    log_instr_hwreg_update(env, mips_cop0_regnames[30],
+                           env->active_tc.CHWR.ErrorEPCC._cr_cursor);
 }
 
 void CHERI_HELPER_IMPL(creadhwr(CPUArchState *env, uint32_t cd, uint32_t hwr))
 {
-    cap_register_t result = *check_readonly_cap_hwr_access(env, CP2HWR_BASE_INDEX + hwr, GETPC());
+    cap_register_t result = *check_readonly_cap_hwr_access(
+        env, CP2HWR_BASE_INDEX + hwr, GETPC());
     update_capreg(env, cd, &result);
 }
 
@@ -506,8 +531,11 @@ void CHERI_HELPER_IMPL(cwritehwr(CPUArchState *env, uint32_t cs, uint32_t hwr))
         update_ddc(env, csp);
         return;
     }
-    cap_register_t *cdp = check_writable_cap_hwr_access(env, CP2HWR_BASE_INDEX + hwr, GETPC());
+    cap_register_t *cdp = check_writable_cap_hwr_access(
+        env, CP2HWR_BASE_INDEX + hwr, GETPC());
     *cdp = *csp;
+    /* Note: DDC updates are logged in update_ddc. */
+    log_changed_capreg(env, mips_cheri_hw_regnames[hwr], csp);
 }
 
 void CHERI_HELPER_IMPL(csetcause(CPUArchState *env, target_ulong rt))
@@ -520,6 +548,7 @@ void CHERI_HELPER_IMPL(csetcause(CPUArchState *env, target_ulong rt))
     } else {
         env->CP2_CapCause = (uint16_t)(rt & 0xffffUL);
     }
+    log_instr_hwreg_update(env, "CapCause", env->CP2_CapCause);
 }
 
 void CHERI_HELPER_IMPL(csetlen(CPUArchState *env, uint32_t cd, uint32_t cb, target_ulong rt))
@@ -797,6 +826,7 @@ void CHERI_HELPER_IMPL(cllc_without_tcg(CPUArchState *env, uint32_t cd, uint32_t
 
 #ifdef CONFIG_CHERI_LOG_INSTR
 
+// TODO(am2419): deprecated, remove
 void dump_changed_capreg(CPUArchState *env, const cap_register_t *cr,
         cap_register_t *old_reg, const char* name)
 {
@@ -817,17 +847,13 @@ void dump_changed_capreg(CPUArchState *env, const cap_register_t *cr,
     }
 }
 
-const char * const cheri_gp_regnames[] = {
-    "C00", "C01", "C02", "C03", "C04", "C05", "C06", "C07",
-    "C08", "C09", "C10", "C11", "C12", "C13", "C14", "C15",
-    "C16", "C17", "C18", "C19", "C20", "C21", "C22", "C23",
-    "C24", "C25", "C26", "C27", "C28", "C29", "C30", "C31",
-};
-
+// TODO(am2419): deprecated, remove
 void dump_changed_cop2(CPUArchState *env, TCState *cur) {
-    dump_changed_capreg(env, &cur->CapBranchTarget, &env->last_CapBranchTarget, "CapBranchTarget");
+    dump_changed_capreg(env, &cur->CapBranchTarget, &env->last_CapBranchTarget,
+                        "CapBranchTarget");
     for (int i=0; i<32; i++) {
-        dump_changed_capreg(env, get_readonly_capreg(env, i), &env->last_C[i], cheri_gp_regnames[i]);
+        dump_changed_capreg(env, get_readonly_capreg(env, i), &env->last_C[i],
+                            cheri_gp_regnames[i]);
     }
     dump_changed_capreg(env, &cur->CHWR.DDC, &env->last_CHWR.DDC, "DDC");
     dump_changed_capreg(env, &cur->CHWR.UserTlsCap, &env->last_CHWR.UserTlsCap, "UserTlsCap");

commit aea704b38a33c5502af4853798f67fb2e4a9f63d

mips: add missing curly brace
Add a curly brace after the last register name. This fixes a compile
error for mipsel-softmmu - and makes compilation fail elsewhere ;-(

Fixes: 8c8daab7270 ("Implment register logging interface.")

target/mips/cpu.c
@@ -265,7 +265,7 @@ const char mips_gp_regnames[32][5] = {
     {"zero"}, {"at"},   {"v0"},   {"v1"},   {"a0"},   {"a1"},   {"a2"},   {"a3"},
     {"t0"},   {"t1"},   {"t2"},   {"t3"},   {"t4"},   {"t5"},   {"t6"},   {"t7"},
     {"s0"},   {"s1"},   {"s2"},   {"s3"},   {"s4"},   {"s5"},   {"s6"},   {"s7"},
-    {"t8"},   {"t9"},   {"k0"},   {"k1"},   {"gp"},   {"sp"},   {"s8"},   {"ra"
+    {"t8"},   {"t9"},   {"k0"},   {"k1"},   {"gp"},   {"sp"},   {"s8"},   {"ra"},
 };
 #else
 // Use n64 register names