CHERI-Alliance/qemu

commit bd656b7b7fbf991731e5e03a4bcf288dba6d3203

cheri: riscv: illegal instruction for gcperm in int ptr mode
The gcperm instruction is allowed only in purecap mode or in hybrid mode
when the system is in capability pointer mode.

Raise an illegal instruction exception if gcperm is called in hybrid,
integer pointer mode.

The gcperm instruction is generic, the exception is riscv-specific. It
seems that we have to bite the bullet and use ifdef here...

target/cheri-common/op_helper_cheri_common.c
@@ -1872,7 +1872,9 @@ target_ulong CHERI_HELPER_IMPL(gcperm(CPUArchState *env, uint32_t cb))
      * cheri_in_capmode will check those combinations.
      */
     if (!cheri_in_capmode(env)) {
-        /* TODO: raise an "illegal instruction" exception */
+#ifdef TARGET_RISCV
+        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
+#endif
 
         /*
          * TODO: What should we return after throwing an exception?

commit 725325ff09e7d06e44f6f2e6879ec08bfd11a659

cheri: remove capmode check for gcperm
The availabilty of the gcperm instruction depends on cheri register
access (the CRE bits), not on capability pointer mode.

The decodetree definition does already do the correct check. Remove the
invalid additional check in the helper function.

Fixes: bd656b7b7fb ("cheri: riscv: illegal instruction for gcperm in int ptr mode")
Fixes: 90ee7e237aa ("cheri: riscv: use bakewell permissions for gcperm")

target/cheri-common/op_helper_cheri_common.c
@@ -1876,29 +1876,6 @@ target_ulong CHERI_HELPER_IMPL(gcperm(CPUArchState *env, uint32_t cb))
     uint8_t ap_bits = 0;
 
     /*
-     * This instruction is allowed only in capability pointer mode, i.e.
-     * purecap or hybrid + M bit + CRE=1 for current CPU mode.
-     * cheri_in_capmode will check those combinations.
-     */
-    if (!cheri_in_capmode(env)) {
-#ifdef TARGET_RISCV
-        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
-#endif
-
-        /*
-         * TODO: What should we return after throwing an exception?
-         * Will the returned value be used at all?
-         *
-         * We looked at
-         * target/mips/op_helper_cheri.c
-         *     CHERI_HELPER_IMPL(ccall_notrap...
-         *     ccall_common
-         * -> they return 0 after throwing an exception
-         */
-        return 0;
-    }
-
-    /*
      * If acperm can't produce the permissions of our input capability, we
      * have to clear all the AP bits in our result.
      */