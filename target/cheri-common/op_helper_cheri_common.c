CHERI-Alliance/qemu

commit 75a27ba2b1740921f5515ec578072b60d148b701

cheri: riscv: illegal instruction for acperm in int ptr mode
The acperm instruction is allowed only in purecap mode or in hybrid mode
when the system is in capability pointer mode.

Raise an illegal instruction exception if acperm is called in hybrid,
integer pointer mode.

The acperm instruction is generic, the exception is riscv-specific. It
seems that we have to bite the bullet and use ifdef here...

target/cheri-common/op_helper_cheri_common.c
@@ -953,7 +953,7 @@ void CHERI_HELPER_IMPL(acperm(CPUArchState *env, uint32_t cd, uint32_t cs1,
 
     /* see the comment about capmode in the gcperm helper */
     if (!cheri_in_capmode(env)) {
-        /* TODO: raise an "illegal instruction" exception */
+        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
         return;
     }
 

commit 91f8fc9eee967edf1edb553aa0b2bbe9aae6e248

cheri: remove capmode check for acperm
The availabilty of the acperm instruction depends on cheri register
access (the CRE bits), not on capability pointer mode.

The decodetree definition does already do the correct check. Remove the
invalid additional check in the helper function.

Fixes: 75a27ba2b17 ("cheri: riscv: illegal instruction for acperm in int ptr mode")
Fixes: 880a2d7228f ("cheri: riscv: replace candperm with acperm")

target/cheri-common/op_helper_cheri_common.c
@@ -958,12 +958,6 @@ void CHERI_HELPER_IMPL(acperm(CPUArchState *env, uint32_t cd, uint32_t cs1,
     uint32_t perms = ((cap_get_sdp(cbp) << 16) | cbp->cr_arch_perm) & rs1;
     cap_register_t result = *cbp;
 
-    /* see the comment about capmode in the gcperm helper */
-    if (!cheri_in_capmode(env)) {
-        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
-        return;
-    }
-
     if (!cap_is_unsealed(cbp)) {
         result.cr_tag = 0;
     }