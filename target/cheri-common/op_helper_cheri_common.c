CHERI-Alliance/qemu

commit c85dbbc4a6c94c8210d87d7980723105ae58cc09

cheri: riscv: illegal instruction for scmode in int ptr mode
The scmode instruction is allowed only in purecap mode or in hybrid mode
when the system is in capability pointer mode.

Raise an illegal instruction exception if scmode is called in hybrid,
integer pointer mode.

The scmode instruction is generic, the exception is riscv-specific. It
seems that we have to bite the bullet and use ifdef here...

target/cheri-common/op_helper_cheri_common.c
@@ -1993,7 +1993,9 @@ void CHERI_HELPER_IMPL(scmode(CPUArchState *env, uint32_t cd, uint32_t cs1,
     cap_register_t result = *csp;
 
     if (!cheri_in_capmode(env)) {
-        /* TODO: Illegal Instruction exception */
+#ifdef TARGET_RISCV
+        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
+#endif
         return;
     }
 

commit 816cb3f4bb3e0a95d27280f724164e39127cc316

cheri: remove capmode check for scmode
The availabilty of the scmode instruction depends on cheri register
access (the CRE bits), not on capability pointer mode.

The decodetree definition does already do the correct check. Remove the
invalid additional check in the helper function.

Fixes: c85dbbc4a6c ("cheri: riscv: illegal instruction for scmode in int ptr mode")
Fixes: dc617320bb6 ("cheri: implement the scmode instruction")

target/cheri-common/op_helper_cheri_common.c
@@ -2142,13 +2142,6 @@ void CHERI_HELPER_IMPL(scmode(CPUArchState *env, uint32_t cd, uint32_t cs1,
     const cap_register_t *csp = get_readonly_capreg(env, cs1);
     cap_register_t result = *csp;
 
-    if (!cheri_in_capmode(env)) {
-#ifdef TARGET_RISCV
-        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
-#endif
-        return;
-    }
-
     if (cd == 0) {
         /*
          * If cd == c0, the result is discarded. The permission check / update