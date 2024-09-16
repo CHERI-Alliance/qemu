CHERI-Alliance/qemu

commit d74ab74581f87b593eba0e3ef8e59fc9b50d5c42

cheri: riscv: add the cbld instruction
Cheri Bakewell renamed the cbuildcap instruction to cbld and modified the
encoding. This instruction takes an authorizing capability and an input
capability. If permitted, a new tagged capability is created from the
input capability. It seems that the related permission checks were
modfied for cheri bakewell.

Reimplement cbld based on the v9 code. Make sure that the checks are in
line with the bakewell specification. Execute cbld only in capability
pointer mode.

For the required malformed checks, we assume that bounds length 0 is
equivalent to a malformed capability.

target/cheri-common/op_helper_cheri_common.c
@@ -1965,6 +1965,126 @@ target_ulong CHERI_HELPER_IMPL(sceq(CPUArchState *env, uint32_t cs1,
     return cap_exactly_equal(cs1p, cs2p);
 }
 
+void CHERI_HELPER_IMPL(cbld(CPUArchState *env, uint32_t cd, uint32_t cs1,
+             uint32_t cs2))
+{
+    const cap_register_t *cs1p = get_readonly_capreg(env, cs1);
+    const cap_register_t *cs2p = get_readonly_capreg(env, cs2);
+    GET_HOST_RETPC_IF_TRAPPING_CHERI_ARCH();
+    cap_register_t result = *cs2p;
+    DEFINE_RESULT_VALID;
+
+    if (!cheri_in_capmode(env)) {
+#ifdef TARGET_RISCV
+        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
+#endif
+        return;
+    }
+
+    if (cs1 == 0) {
+        /*
+         * If cs1 is the c0 register, we have to copy cs2 to cd and clear
+         * cd's tag.
+         */
+        _cap_valid = false;
+    }
+    else if (!cs1p->cr_tag) {
+        raise_cheri_exception_or_invalidate(env, CapEx_TagViolation,
+                CapExType_Data, cs1);
+    }
+    else if (cap_get_length_full(cs1p) == 0) {
+        /*
+         * In cheri bakewell, malformed bounds decode as base 0, top 0.
+         * We take length = top - base = 0 as an indication for a malformed
+         * capability.
+         */
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs1);
+    } else if (cap_has_reserved_bits_set(cs1p)) {
+        /* None of the exception types seems like a really good fit here... */
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs1);
+    } else if (!cap_is_unsealed(cs1p)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_SealViolation,
+                CapExType_Data, cs1);
+    } else if ((cs2p->cr_arch_perm & cs1p->cr_arch_perm) != cs2p->cr_arch_perm) {
+        /*
+         * There's no need to decompress cs1p or cs2p manually.
+         * get_readonly_capreg returns a fully decompressed capability.
+         */
+        raise_cheri_exception_or_invalidate(env, CapEx_PermissionViolation,
+                CapExType_Data, cs1);
+    } else if (!valid_ap(cs2p->cr_arch_perm)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_PermissionViolation,
+                CapExType_Data, cs2);
+    } else if ((cap_get_sdp(cs2p) & cap_get_sdp(cs1p)) != cap_get_sdp(cs2p)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_PermissionViolation,
+                CapExType_Data, cs1);
+    } else if (cap_get_base(cs2p) < cap_get_base(cs1p)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs1);
+    } else if (cap_get_top_full(cs2p) > cap_get_top_full(cs1p)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs1);
+    } else if (cap_get_base(cs2p) > cap_get_top_full(cs2p)) {
+        // check for length < 0 - possible because cs2 might be untagged
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs2);
+    } else if (cap_get_length_full(cs2p) == 0) {
+        /* see the comment above about the malformed check */
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs2);
+    } else if (cap_has_reserved_bits_set(cs2p)) {
+        raise_cheri_exception_or_invalidate(env, CapEx_LengthViolation,
+                CapExType_Data, cs2);
+    }
+
+    /*
+     * The v9 code for cbuildcap unsealed result at this point and sealed it
+     * again immediately afterwards if cs2 is sealed.
+     * result is a copy of cs2, the sealed bit was copied as well. We believe
+     * that there's no need for manually updating it here.
+     */
+
+    if (!RESULT_VALID) {
+        result.cr_tag = 0; /* Not a valid subset. */
+    } else {
+        /* Check if the capability bounds are canonical by deriving. */
+        cap_register_t derived = *cs1p;
+        if (!cap_is_unsealed(&derived)) {
+            derived.cr_tag = 0;
+        }
+        cap_set_cursor(&derived, cap_get_base(&result));
+        CAP_cc(setbounds)(&derived, cap_get_length_full(&result));
+        cap_set_cursor(&derived, cap_get_cursor(&result));
+
+        /* We checked above that
+           cs2p->cr_arch_perm & cs1p->cr_arch_perm) == cs2p->cr_arch_perm */
+        derived.cr_arch_perm = cs2p->cr_arch_perm;
+        CAP_cc(ap_compress)(&derived);
+
+        /* We checked above that
+           (cap_get_sdp(cs2p) & cap_get_sdp(cs1p) == cap_get_sdp(cs2p) */
+        CAP_cc(update_sdp)(&derived, cap_get_sdp(cs2p));
+
+        if (!cap_is_unsealed(cs2p)) {
+            cap_set_sealed(&derived, SEALED_TYPE_UNUSED);
+        }
+        result.cr_tag = 1; /* Set tag to true for comparison with derived. */
+        if (cap_exactly_equal(&result, &derived)) {
+            /*
+             * If this was a valid derivation sequence return that to ensure
+             * canonical bounds encoding.
+             */
+            result = derived;
+        } else {
+            /* Valid subset but not canonical -> return the untagged input. */
+            result.cr_tag = 0;
+        }
+    }
+    update_capreg(env, cd, &result);
+}
+
 void CHERI_HELPER_IMPL(sentry(CPUArchState *env, uint32_t cd, uint32_t cs1))
 {
     GET_HOST_RETPC_IF_TRAPPING_CHERI_ARCH();

disas/riscv.c
@@ -535,6 +535,7 @@ typedef enum {
     rv_op_schi,
     rv_op_sceq,
     rv_op_scmode,
+    rv_op_cbld,
 
     // FP loads/store
     rv_op_cflw,
@@ -1263,6 +1264,7 @@ const rv_opcode_data opcode_data[] = {
     [rv_op_schi] = { "schi", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
     [rv_op_sceq] = { "sceq", rv_codec_r, rv_fmt_rd_cs1_cs2, NULL, 0, 0, 0 },
     [rv_op_scmode] = { "scmode", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
+    [rv_op_cbld] = { "cbld", rv_codec_r, rv_fmt_cd_cs1_rs2, NULL, 0, 0, 0 },
 
     // FP load store
     [rv_op_cflw] = { "cflw", rv_codec_i, rv_fmt_frd_offset_cs1, NULL, 0, 0, 0 },
@@ -1855,6 +1857,7 @@ static void decode_inst_opcode(rv_decode *dec, rv_isa isa, int flags)
             case 50: op = rv_op_acperm; break;
             case 51: op = rv_op_schi; break;
             case 52: op = rv_op_sceq; break;
+            case 53: op = rv_op_cbld; break;
             case 55: op = rv_op_scmode; break;
             case 56: op = rv_op_scbnds; break;
             case 57: op = rv_op_scbndsr; break;

target/cheri-common/cheri-helper-common.h
@@ -160,6 +160,7 @@ DEF_HELPER_4(caddi, void, env, i32, i32, tl)
 DEF_HELPER_4(scaddr, void, env, i32, i32, tl)
 DEF_HELPER_4(schi, void, env, i32, i32, tl)
 DEF_HELPER_3(sceq, tl, env, i32, i32)
+DEF_HELPER_4(cbld, void, env, i32, i32, i32)
 DEF_HELPER_3(sentry, void, env, i32, i32)
 
 DEF_HELPER_4(scmode, void, env, i32, i32, tl)

target/riscv/insn32-cheri.decode
@@ -59,12 +59,14 @@ ctestsubset 0100000  ..... ..... 000 ..... 1011011 @r
 
 @rcc     .......   ..... ..... ... ..... ....... %rs2 %cs1 %cd
 @ccr     .......   ..... ..... ... ..... ....... %cs2 %cs1 %rd
+@ccc     .......   ..... ..... ... ..... ....... %cs2 %cs1 %cd
 
 cadd   0000110  ..... ..... 000 ..... 0110011 @rcc
 scaddr 0000110  ..... ..... 001 ..... 0110011 @rcc
 schi   0000110  ..... ..... 011 ..... 0110011 @rcc
 sceq   0000110  ..... ..... 100 ..... 0110011 @ccr
 acperm 0000110  ..... ..... 010 ..... 0110011 @rcc
+cbld   0000110  ..... ..... 101 ..... 0110011 @ccc
 scmode 0000110  ..... ..... 111 ..... 0110011 @rcc
 
 

target/riscv/insn_trans/trans_cheri.c.inc
@@ -691,6 +691,9 @@ TRANSLATE_SC_EXPLICIT(sc_d, MO_ALIGN | MO_Q);
 #define TRANSLATE_CAP_CAP_BW(name) \
     DO_TRANSLATE(name, gen_cheri_cap_cap, a->cd, a->cs1)
 
+#define TRANSLATE_CAP_CAP_CAP_BW(name) \
+    DO_TRANSLATE(name, gen_cheri_cap_cap_cap, a->cd, a->cs1, a->cs2)
+
 TRANSLATE_INT_CAP_BW(gctag)
 TRANSLATE_INT_CAP_BW(gcperm)
 TRANSLATE_INT_CAP_BW(gchi)
@@ -764,6 +767,7 @@ TRANSLATE_CAP_CAP_INT_BW(scaddr)
 TRANSLATE_CAP_CAP_INT_BW(schi)
 TRANSLATE_INT_CAP_CAP_BW(sceq)
 TRANSLATE_CAP_CAP_INT_BW(acperm)
+TRANSLATE_CAP_CAP_CAP_BW(cbld)
 
 TRANSLATE_CAP_CAP_BW(sentry)
 

commit 5ceb2a5f5e827d3e99846a6917c4e6d76c3a60cb

cheri: remove capmode check for cbld
The availabilty of the cbld instruction depends on cheri register
access (the CRE bits), not on capability pointer mode.

The decodetree definition does already do the correct check. Remove the
invalid additional check in the helper function.

Fixes: d74ab74581f ("cheri: riscv: add the cbld instruction")

target/cheri-common/op_helper_cheri_common.c
@@ -1959,13 +1959,6 @@ void CHERI_HELPER_IMPL(cbld(CPUArchState *env, uint32_t cd, uint32_t cs1,
     cap_register_t result = *cs2p;
     DEFINE_RESULT_VALID;
 
-    if (!cheri_in_capmode(env)) {
-#ifdef TARGET_RISCV
-        riscv_raise_exception(env, RISCV_EXCP_ILLEGAL_INST, GETPC());
-#endif
-        return;
-    }
-
     if (cs1 == 0) {
         /*
          * If cs1 is the c0 register, we have to copy cs2 to cd and clear