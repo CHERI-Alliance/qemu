CHERI-Alliance/qemu

commit 8c0a8951ccdbec1764ac3f8864e1d4152eb04604

cheri: use cr_m to access the M bit
Update the qemu cheri code to access a capability's M bit via cr_m.

Using the CAP_AP_M bit in cr_arch_perm is no longer supported by
the cheri-compressed-cap library.

target/cheri-common/cheri_utils.h
@@ -186,7 +186,7 @@ static inline bool cap_get_capmode(const cap_register_t *c)
     return CAP_cc(get_flags)(c);
 #else
     CAP_cc(ap_decompress)((cap_register_t *)c);
-    return c->cr_arch_perm & CAP_AP_M;
+    return c->cr_m;
 #endif
 }
 
@@ -196,8 +196,7 @@ static inline void cap_set_capmode(cap_register_t *c, bool enable)
     CAP_cc(update_flags)(c, enable ? 1 : 0);
 #else
     CAP_cc(ap_decompress)((cap_register_t *)c);
-    c->cr_arch_perm &= ~CAP_AP_M;
-    c->cr_arch_perm |= enable ? CAP_AP_M : 0;
+    c->cr_m = 1;
     CAP_cc(ap_compress)((cap_register_t *)c);
 #endif
 }

target/cheri-common/op_helper_cheri_common.c
@@ -955,9 +955,7 @@ void CHERI_HELPER_IMPL(acperm(CPUArchState *env, uint32_t cd, uint32_t cs1,
                               target_ulong rs1))
 {
     const cap_register_t *cbp = get_readonly_capreg(env, cs1);
-    /* We do not touch the M bit here, don't copy it into perms. */
-    uint32_t perms =
-        ((cap_get_sdp(cbp) << 16) | (cbp->cr_arch_perm & ~CAP_AP_M)) & rs1;
+    uint32_t perms = ((cap_get_sdp(cbp) << 16) | cbp->cr_arch_perm) & rs1;
     cap_register_t result = *cbp;
 
     /* see the comment about capmode in the gcperm helper */
@@ -1905,8 +1903,7 @@ target_ulong CHERI_HELPER_IMPL(gcperm(CPUArchState *env, uint32_t cb))
     if (!valid_ap(cbp->cr_arch_perm))
         goto out;
 
-    /* The M bit is not part of gcperm's bitfield. */
-    ap_bits = (cbp->cr_arch_perm & ~CAP_AP_M);
+    ap_bits = cbp->cr_arch_perm;
 
 out:
     return (cap_get_sdp(cbp) << 16) | ap_bits;
@@ -2193,9 +2190,7 @@ void CHERI_HELPER_IMPL(scmode(CPUArchState *env, uint32_t cd, uint32_t cs1,
         result.cr_tag = 0;
 
     if (valid_ap(csp->cr_arch_perm) && (csp->cr_arch_perm & CAP_AP_X)) {
-        result.cr_arch_perm = (rs2 & 0b1) ?
-            (result.cr_arch_perm | CAP_AP_M) : (result.cr_arch_perm & ~CAP_AP_M);
-
+        result.cr_m = rs2 & 0x01;
         CAP_cc(ap_compress)(&result);
     }
 

commit e24da598b2038a2eb557cbb09061a4db5a6f6b03

cheri: process the enable parameter in cap_set_capmode
Commit 8c0a8951ccd ("cheri: use cr_m to access the M bit") split M and
AP - and dropped the enable parameter by accident. Bring it back instead
of assuming that we always want to turn capmode on.

Fixes: 8c0a8951ccd ("cheri: use cr_m to access the M bit")

target/cheri-common/cheri_utils.h
@@ -196,7 +196,7 @@ static inline void cap_set_capmode(cap_register_t *c, bool enable)
     CAP_cc(update_flags)(c, enable ? 1 : 0);
 #else
     CAP_cc(m_ap_decompress)((cap_register_t *)c);
-    c->cr_m = 0;
+    c->cr_m = enable ? 0 : 1;
     CAP_cc(m_ap_compress)((cap_register_t *)c);
 #endif
 }