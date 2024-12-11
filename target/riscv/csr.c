b96dc5f16159b507538a36e7fe98c79977880f48
Paul Buxton <paul.buxton@codasip.com>
2024-12-11 09:16:09 +0000

    Add the sstatus_crg bit The SSTATUS.CRG bit is a new bit in the SStatus register used as part of the ZCheriPTE specification. We add the bit to the M and S set of defines and the masks used to filter writes to the M and S versions of the register.

--- target/riscv/csr.c
+++ target/riscv/csr.c
@@ -540,7 +540,7 @@ static const target_ulong sstatus_v1_10_mask = SSTATUS_SIE | SSTATUS_SPIE |
     SSTATUS_UIE | SSTATUS_UPIE | SSTATUS_SPP | SSTATUS_FS | SSTATUS_XS |
     SSTATUS_SUM | SSTATUS_MXR
 #if defined(TARGET_RISCV64)
-    | SSTATUS64_UXL
+    | SSTATUS64_UXL | SSTATUS64_CRG
 #endif
     ;
 static const target_ulong sip_writable_mask = SIP_SSIP | MIP_USIP | MIP_UEIP;
@@ -622,9 +622,11 @@ static RISCVException write_mstatus(CPURISCVState *env, int csrno,
         tlb_flush(env_cpu(env));
     }
     mask = MSTATUS_SIE | MSTATUS_SPIE | MSTATUS_MIE | MSTATUS_MPIE |
-        MSTATUS_SPP | MSTATUS_FS | MSTATUS_MPRV | MSTATUS_SUM |
-        MSTATUS_MPP | MSTATUS_MXR | MSTATUS_TVM | MSTATUS_TSR |
-        MSTATUS_TW;
+           MSTATUS_SPP | MSTATUS_FS | MSTATUS_MPRV | MSTATUS_SUM | MSTATUS_MPP |
+           MSTATUS_MXR | MSTATUS_TVM | MSTATUS_TSR | MSTATUS_TW;
+#if defined(TARGET_CHERI) && !defined(TARGET_RISCV32)
+            mask = mask | MSTATUS64_CRG;
+#endif            
 
     if (riscv_cpu_mxl(env) != MXL_RV32) {
         /*
