c79342309ae74155c0071ff58d7b0c515e1d14d0
Paul Buxton <paul.buxton@codasip.com>
2024-12-11 08:54:51 +0000

    The ZCheriPTE extension is a required part of CHERI if virtual memory is used. however introducing it directly would break kernels that do not have support for it yet, as it expects bits set in the PTE entry. So we add a flag that can be used to enable it when required. This will be removed / default to on later.

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -1137,6 +1137,7 @@ static Property riscv_cpu_properties[] = {
     DEFINE_PROP_BOOL("scmode_flip", RISCVCPU, cfg.m_flip, false),
     DEFINE_PROP_BOOL("m_flip", RISCVCPU, cfg.m_flip, false),
     DEFINE_PROP_BOOL("cheri_v090", RISCVCPU, cfg.cheri_v090, false),
+    DEFINE_PROP_BOOL("cheri_pte", RISCVCPU, cfg.cheri_pte, false),
 #endif
 
     DEFINE_PROP_STRING("vext_spec", RISCVCPU, cfg.vext_spec),
