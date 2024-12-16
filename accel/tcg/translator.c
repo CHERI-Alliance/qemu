CHERI-Alliance/qemu

commit 6b188a6463dd1df95a90552ae1b20394bec21e36

Avoid using cs_base for CHERI
Commit a378206a "target/arm: Move mode specific TB flags to tb->cs_base"
now uses this field for additional TB flags as it was unused in the Arm
target before. We were using this field downstream to store the base of
PCC (which IMO is appropriate considering how it is used in other targets).

While we could reuse cs_base to store the CHERI flags now, this commit uses
a simpler approach of adding another field for pcc_base to avoid future
merge conflicts. This can be cleaned up once we have caught up with latest
QEMU.

accel/tcg/translator.c
@@ -58,7 +58,7 @@ void translator_loop(const TranslatorOps *ops, DisasContextBase *db,
     db->max_insns = max_insns;
     db->singlestep_enabled = cpu->singlestep_enabled;
 #ifdef TARGET_CHERI
-    db->pcc_base = tb->cs_base;
+    db->pcc_base = tb->pcc_base;
     db->pcc_top = tb->cs_top;
     cheri_debug_assert(db->pcc_base ==
                        cap_get_base(cheri_get_recent_pcc(cpu->env_ptr)));

target/arm/cpu.h
@@ -3821,6 +3821,14 @@ extern void aarch_cpu_get_tb_cpu_state(CPUARMState *env, target_ulong *pc,
 #define EX_TBFLAG_M32(IN, WHICH)   FIELD_EX32(IN.flags2, TBFLAG_M32, WHICH)
 #define EX_TBFLAG_AM32(IN, WHICH)  FIELD_EX32(IN.flags2, TBFLAG_AM32, WHICH)
 
+extern void aarch_cpu_get_tb_cpu_state(CPUARMState *env, target_ulong *pc,
+                                       target_ulong *cs_base,
+                                       target_ulong *pcc_base,
+                                       target_ulong *pcc_top,
+                                       uint32_t *cheri_flags, uint32_t *pflags);
+// Ugly macro hack to avoid having to modify cpu_get_tb_cpu_state in all targets
+#define cpu_get_tb_cpu_state_ext aarch_cpu_get_tb_cpu_state
+
 /**
  * cpu_mmu_index:
  * @env: The cpu environment

target/mips/cpu.h
@@ -1480,21 +1480,20 @@ target_ulong exception_resume_pc(CPUMIPSState *env);
 
 static inline void
 mips_cpu_get_tb_cpu_state(CPUMIPSState *env, target_ulong *pc,
-                          target_ulong *cs_base, target_ulong *cs_top,
-                          uint32_t *cheri_flags, uint32_t *flags)
+                          target_ulong *cs_base, target_ulong *pcc_base,
+                          target_ulong *pcc_top, uint32_t *cheri_flags,
+                          uint32_t *flags)
 {
     *pc = PC_ADDR(env); // We want the full virtual address here (no offset)
     *flags = env->hflags &
              (MIPS_HFLAG_TMASK | MIPS_HFLAG_BMASK | MIPS_HFLAG_HWRENA_ULR);
 #ifdef TARGET_CHERI
-    cheri_cpu_get_tb_cpu_state(env, &env->active_tc.PCC, &env->active_tc.CHWR.DDC,
-                               cs_base, cs_top, cheri_flags);
-#else
-    *cs_base = 0;
+    cheri_cpu_get_tb_cpu_state(&env->active_tc.PCC, &env->active_tc.CHWR.DDC,
+                               pcc_base, pcc_top, cheri_flags);
 #endif
 }
 // Ugly macro hack to avoid having to modify cpu_get_tb_cpu_state in all targets
-#define cpu_get_tb_cpu_state_6 mips_cpu_get_tb_cpu_state
+#define cpu_get_tb_cpu_state_ext mips_cpu_get_tb_cpu_state
 
 static inline bool should_use_error_epc(CPUMIPSState *env)
 {

target/riscv/cpu.h
@@ -721,13 +721,14 @@ static inline uint32_t vext_get_vlmax(RISCVCPU *cpu, target_ulong vtype)
 
 static inline void
 riscv_cpu_get_tb_cpu_state(CPURISCVState *env, target_ulong *pc,
-                           target_ulong *cs_base, target_ulong *cs_top,
-                           uint32_t *cheri_flags, uint32_t *pflags)
+                           target_ulong *cs_base, target_ulong *pcc_base,
+                           target_ulong *pcc_top, uint32_t *cheri_flags,
+                           uint32_t *pflags)
 {
     uint32_t flags = 0;
     *pc = PC_ADDR(env); // We want the full virtual address here (no offset)
 #ifdef TARGET_CHERI
-    cheri_cpu_get_tb_cpu_state(env, &env->PCC, &env->DDC, cs_base, cs_top,
+    cheri_cpu_get_tb_cpu_state(&env->PCC, &env->DDC, pcc_base, pcc_top,
                                cheri_flags);
 #else
     *cs_base = 0;
@@ -767,7 +768,7 @@ riscv_cpu_get_tb_cpu_state(CPURISCVState *env, target_ulong *pc,
     *pflags = flags;
 }
 // Ugly macro hack to avoid having to modify cpu_get_tb_cpu_state in all targets
-#define cpu_get_tb_cpu_state_6 riscv_cpu_get_tb_cpu_state
+#define cpu_get_tb_cpu_state_ext riscv_cpu_get_tb_cpu_state
 
 #ifdef CONFIG_TCG_LOG_INSTR
 #define RISCV_LOG_INSTR_CPU_U QEMU_LOG_INSTR_CPU_USER

accel/tcg/cpu-exec.c
@@ -166,10 +166,10 @@ cpu_tb_exec(CPUState *cpu, TranslationBlock *itb, int *tb_exit)
 
     qemu_log_mask_and_addr(CPU_LOG_EXEC, itb->pc,
                            "Trace %d: %p [" TARGET_FMT_lx "/" TARGET_FMT_lx
-                           "/" TARGET_FMT_lx "/%#x/%#x] %s\n",
+                           "/" TARGET_FMT_lx "-" TARGET_FMT_lx "/%#x/%#x] %s\n",
                            cpu->cpu_index, itb->tc.ptr, itb->cs_base, itb->pc,
-                           itb->cs_top, itb->cheri_flags, itb->flags,
-                           lookup_symbol(itb->pc));
+                           itb->pcc_base, itb->pcc_top, itb->cheri_flags,
+                           itb->flags, lookup_symbol(itb->pc));
 
 #if defined(DEBUG_DISAS)
     if (qemu_loglevel_mask(CPU_LOG_TB_CPU)
@@ -247,7 +247,7 @@ void cpu_exec_step_atomic(CPUState *cpu)
 {
     CPUArchState *env = (CPUArchState *)cpu->env_ptr;
     TranslationBlock *tb;
-    target_ulong cs_base, cs_top = 0, pc;
+    target_ulong cs_base, pcc_base = 0, pcc_top = 0, pc;
     uint32_t cheri_flags = 0;
     uint32_t flags;
     uint32_t cflags = (curr_cflags(cpu) & ~CF_PARALLEL) | 1;
@@ -259,13 +259,15 @@ void cpu_exec_step_atomic(CPUState *cpu)
         g_assert(!cpu->running);
         cpu->running = true;
 
-        cpu_get_tb_cpu_state_6(env, &pc, &cs_base, &cs_top, &cheri_flags, &flags);
-        tb = tb_lookup(cpu, pc, cs_base, cs_top, cheri_flags, flags, cflags);
+        cpu_get_tb_cpu_state_ext(env, &pc, &cs_base, &pcc_base, &pcc_top,
+                                 &cheri_flags, &flags);
+        tb = tb_lookup(cpu, pc, cs_base, pcc_base, pcc_top, cheri_flags, flags,
+                       cflags);
 
         if (tb == NULL) {
             mmap_lock();
-            tb = tb_gen_code(cpu, pc, cs_base, cs_top, cheri_flags, flags,
-                             cflags);
+            tb = tb_gen_code(cpu, pc, cs_base, pcc_base, pcc_top, cheri_flags,
+                             flags, cflags);
             mmap_unlock();
         }
 
@@ -303,7 +305,8 @@ void cpu_exec_step_atomic(CPUState *cpu)
 struct tb_desc {
     target_ulong pc;
     target_ulong cs_base;
-    target_ulong cs_top;
+    target_ulong pcc_base;
+    target_ulong pcc_top;
     CPUArchState *env;
     tb_page_addr_t phys_page1;
     uint32_t cheri_flags;
@@ -318,8 +321,9 @@ static bool tb_lookup_cmp(const void *p, const void *d)
     const struct tb_desc *desc = d;
 
     if (tb->pc == desc->pc && tb->page_addr[0] == desc->phys_page1 &&
-        tb->cs_base == desc->cs_base && tb->cs_top == desc->cs_top &&
-        tb->cheri_flags == desc->cheri_flags && tb->flags == desc->flags &&
+        tb->cs_base == desc->cs_base && tb->pcc_base == desc->pcc_base &&
+        tb->pcc_top == desc->pcc_top && tb->cheri_flags == desc->cheri_flags &&
+        tb->flags == desc->flags &&
         tb->trace_vcpu_dstate == desc->trace_vcpu_dstate &&
         tb_cflags(tb) == desc->cflags) {
         /* check next page if needed */
@@ -340,9 +344,9 @@ static bool tb_lookup_cmp(const void *p, const void *d)
 }
 
 TranslationBlock *tb_htable_lookup(CPUState *cpu, target_ulong pc,
-                                   target_ulong cs_base, target_ulong cs_top,
-                                   uint32_t cheri_flags, uint32_t flags,
-                                   uint32_t cflags)
+                                   target_ulong cs_base, target_ulong pcc_base,
+                                   target_ulong pcc_top, uint32_t cheri_flags,
+                                   uint32_t flags, uint32_t cflags)
 {
     tb_page_addr_t phys_pc;
     struct tb_desc desc;
@@ -350,7 +354,8 @@ TranslationBlock *tb_htable_lookup(CPUState *cpu, target_ulong pc,
 
     desc.env = (CPUArchState *)cpu->env_ptr;
     desc.cs_base = cs_base;
-    desc.cs_top = cs_top;
+    desc.pcc_base = pcc_base;
+    desc.pcc_top = pcc_top;
     desc.cheri_flags = cheri_flags;
     desc.flags = flags;
     desc.cflags = cflags;
@@ -425,16 +430,19 @@ static inline TranslationBlock *tb_find(CPUState *cpu,
 {
     CPUArchState *env = (CPUArchState *)cpu->env_ptr;
     TranslationBlock *tb;
-    target_ulong cs_base, cs_top = 0, pc;
+    target_ulong cs_base, pcc_base = 0, pcc_top = 0, pc;
     uint32_t cheri_flags = 0;
     uint32_t flags;
 
-    cpu_get_tb_cpu_state_6(env, &pc, &cs_base, &cs_top, &cheri_flags, &flags);
+    cpu_get_tb_cpu_state_ext(env, &pc, &cs_base, &pcc_base, &pcc_top,
+                             &cheri_flags, &flags);
 
-    tb = tb_lookup(cpu, pc, cs_base, cs_top, cheri_flags, flags, cflags);
+    tb = tb_lookup(cpu, pc, cs_base, pcc_base, pcc_top, cheri_flags, flags,
+                   cflags);
     if (tb == NULL) {
         mmap_lock();
-        tb = tb_gen_code(cpu, pc, cs_base, cs_top, cheri_flags, flags, cflags);
+        tb = tb_gen_code(cpu, pc, cs_base, pcc_base, pcc_top, cheri_flags,
+                         flags, cflags);
         mmap_unlock();
         /* We add the TB in the virtual pc hash table for the fast lookup */
         qatomic_set(&cpu->tb_jmp_cache[tb_jmp_cache_hash_func(pc)], tb);

accel/tcg/internal.h
@@ -12,8 +12,9 @@
 #include "exec/exec-all.h"
 
 TranslationBlock *tb_gen_code(CPUState *cpu, target_ulong pc,
-                              target_ulong cs_base, target_ulong cs_top,
-                              uint32_t cheri_flags, uint32_t flags, int cflags);
+                              target_ulong cs_base, target_ulong pcc_base,
+                              target_ulong pcc_top, uint32_t cheri_flags,
+                              uint32_t flags, int cflags);
 
 void QEMU_NORETURN cpu_io_recompile(CPUState *cpu, uintptr_t retaddr);
 

accel/tcg/tcg-runtime.c
@@ -154,21 +154,23 @@ const void *HELPER(lookup_tb_ptr)(CPUArchState *env)
 {
     CPUState *cpu = env_cpu(env);
     TranslationBlock *tb;
-    target_ulong cs_base, cs_top = 0, pc;
+    target_ulong cs_base, pcc_base = 0, pcc_top = 0, pc;
     uint32_t cheri_flags = 0;
     uint32_t flags;
 
-    cpu_get_tb_cpu_state_6(env, &pc, &cs_base, &cs_top, &cheri_flags, &flags);
+    cpu_get_tb_cpu_state_ext(env, &pc, &cs_base, &pcc_base, &pcc_top,
+                             &cheri_flags, &flags);
 
-    tb = tb_lookup(cpu, pc, cs_base, cs_top, cheri_flags, flags, curr_cflags(cpu));
+    tb = tb_lookup(cpu, pc, cs_base, pcc_base, pcc_top, cheri_flags, flags,
+                   curr_cflags(cpu));
     if (tb == NULL) {
         return tcg_code_gen_epilogue;
     }
     qemu_log_mask_and_addr(CPU_LOG_EXEC, pc,
                            "Chain %d: %p [" TARGET_FMT_lx "/" TARGET_FMT_lx
-                           "/" TARGET_FMT_lx "/%#x/%#x] %s\n",
-                           cpu->cpu_index, tb->tc.ptr, cs_base, pc, cs_top,
-                           cheri_flags, flags, lookup_symbol(pc));
+                           "/" TARGET_FMT_lx "-" TARGET_FMT_lx "/%#x/%#x] %s\n",
+                           cpu->cpu_index, tb->tc.ptr, cs_base, pc, pcc_base,
+                           pcc_top, cheri_flags, flags, lookup_symbol(pc));
     return tb->tc.ptr;
 }
 

accel/tcg/translate-all.c
@@ -1309,7 +1309,8 @@ static bool tb_cmp(const void *ap, const void *bp)
 
     return a->pc == b->pc &&
         a->cs_base == b->cs_base &&
-        a->cs_top == b->cs_top &&
+        a->pcc_base == b->pcc_base &&
+        a->pcc_top == b->pcc_top &&
         a->cheri_flags == b->cheri_flags &&
         a->flags == b->flags &&
         (tb_cflags(a) & ~CF_INVALID) == (tb_cflags(b) & ~CF_INVALID) &&
@@ -1844,8 +1845,9 @@ tb_link_page(TranslationBlock *tb, tb_page_addr_t phys_pc,
 
 /* Called with mmap_lock held for user mode emulation.  */
 TranslationBlock *tb_gen_code(CPUState *cpu, target_ulong pc,
-                              target_ulong cs_base, target_ulong cs_top,
-                              uint32_t cheri_flags, uint32_t flags, int cflags)
+                              target_ulong cs_base, target_ulong pcc_base,
+                              target_ulong pcc_top, uint32_t cheri_flags,
+                              uint32_t flags, int cflags)
 {
     CPUArchState *env = cpu->env_ptr;
     TranslationBlock *tb, *existing_tb;
@@ -1894,7 +1896,8 @@ TranslationBlock *tb_gen_code(CPUState *cpu, target_ulong pc,
     tb->tc.ptr = tcg_splitwx_to_rx(gen_code_buf);
     tb->pc = pc;
     tb->cs_base = cs_base;
-    tb->cs_top = cs_top;
+    tb->pcc_base = pcc_base;
+    tb->pcc_top = pcc_top;
     tb->cheri_flags = cheri_flags;
     tb->flags = flags;
     tb->cflags = cflags;
@@ -2134,7 +2137,8 @@ tb_invalidate_phys_page_range__locked(struct page_collection *pages,
     TranslationBlock *current_tb = NULL;
     target_ulong current_pc = 0;
     target_ulong current_cs_base = 0;
-    target_ulong current_cs_top = 0;
+    target_ulong current_pcc_base = 0;
+    target_ulong current_pcc_top = 0;
     uint32_t current_cheri_flags = 0;
     uint32_t current_flags = 0;
 #endif /* TARGET_HAS_PRECISE_SMC */
@@ -2180,9 +2184,9 @@ tb_invalidate_phys_page_range__locked(struct page_collection *pages,
                  */
                 current_tb_modified = true;
                 cpu_restore_state_from_tb(cpu, current_tb, retaddr, true);
-                cpu_get_tb_cpu_state_6(env, &current_pc, &current_cs_base,
-                                     &current_cs_top, &current_cheri_flags,
-                                     &current_flags);
+                cpu_get_tb_cpu_state_ext(env, &current_pc, &current_cs_base,
+                                         &current_pcc_base, &current_pcc_top,
+                                         &current_cheri_flags, &current_flags);
             }
 #endif /* TARGET_HAS_PRECISE_SMC */
             tb_phys_invalidate__locked(tb);
@@ -2325,8 +2329,8 @@ static bool tb_invalidate_phys_page(tb_page_addr_t addr, uintptr_t pc)
     int current_tb_modified = 0;
     target_ulong current_pc = 0;
     target_ulong current_cs_base = 0;
-    target_ulong current_cs_top = 0;
-    target_ulong current_ds_base = 0;
+    target_ulong current_pcc_base = 0;
+    target_ulong current_pcc_top = 0;
     uint32_t current_cheri_flags = 0;
     uint32_t current_flags = 0;
 #endif
@@ -2360,9 +2364,9 @@ static bool tb_invalidate_phys_page(tb_page_addr_t addr, uintptr_t pc)
 
             current_tb_modified = 1;
             cpu_restore_state_from_tb(cpu, current_tb, pc, true);
-            cpu_get_tb_cpu_state_6(env, &current_pc, &current_cs_base,
-                                 &current_cs_top, &current_cheri_flags,
-                                 &current_flags);
+            cpu_get_tb_cpu_state_ext(env, &current_pc, &current_cs_base,
+                                     &current_pcc_base, &current_pcc_top,
+                                     &current_cheri_flags, &current_flags);
         }
 #endif /* TARGET_HAS_PRECISE_SMC */
         tb_phys_invalidate(tb, addr);
@@ -2396,12 +2400,13 @@ void tb_check_watchpoint(CPUState *cpu, uintptr_t retaddr)
         /* The exception probably happened in a helper.  The CPU state should
            have been saved before calling it. Fetch the PC from there.  */
         CPUArchState *env = cpu->env_ptr;
-        target_ulong pc, cs_base, cs_top = 0;
+        target_ulong pc, cs_base, pcc_base = 0, pcc_top = 0;
         uint32_t cheri_flags = 0;
         tb_page_addr_t addr;
         uint32_t flags;
 
-        cpu_get_tb_cpu_state_6(env, &pc, &cs_base, &cs_top, &cheri_flags, &flags);
+        cpu_get_tb_cpu_state_ext(env, &pc, &cs_base, &pcc_base, &pcc_top,
+                                 &cheri_flags, &flags);
         addr = get_page_addr_code(env, pc);
         if (addr != -1) {
             tb_invalidate_phys_range(addr, addr + 1);

commit d15e7c0e91ab5d99b95ae5013963e8b073474231

arm: cheri: fix "Avoid using cs_base for CHERI"
Alex' patch "Avoid using cs_base for CHERI" renamed cs_top in struct
tb_desc to pcc_top. Fix up one caller where cs_top wasn't renamed.

Don't pull in a duplicate definition of aarch_cpu_get_tb_cpu_state.

Fixes: 6b188a6463d ("Avoid using cs_base for CHERI")

accel/tcg/translator.c
@@ -59,7 +59,7 @@ void translator_loop(const TranslatorOps *ops, DisasContextBase *db,
     db->singlestep_enabled = cpu->singlestep_enabled;
 #ifdef TARGET_CHERI
     db->pcc_base = tb->pcc_base;
-    db->pcc_top = tb->cs_top;
+    db->pcc_top = tb->pcc_top;
     cheri_debug_assert(db->pcc_base ==
                        cap_get_base(cheri_get_recent_pcc(cpu->env_ptr)));
     cheri_debug_assert(db->pcc_top ==

target/arm/cpu.h
@@ -3794,13 +3794,6 @@ FIELD(TBFLAG_A64, TCMA, 16, 2)
 FIELD(TBFLAG_A64, MTE_ACTIVE, 18, 1)
 FIELD(TBFLAG_A64, MTE0_ACTIVE, 19, 1)
 
-extern void aarch_cpu_get_tb_cpu_state(CPUARMState *env, target_ulong *pc,
-                                       target_ulong *cs_base,
-                                       target_ulong *cs_top,
-                                       uint32_t *cheri_flags, uint32_t *pflags);
-// Ugly macro hack to avoid having to modify cpu_get_tb_cpu_state in all targets
-#define cpu_get_tb_cpu_state_6 aarch_cpu_get_tb_cpu_state
-
 /*
  * Helpers for using the above.
  */