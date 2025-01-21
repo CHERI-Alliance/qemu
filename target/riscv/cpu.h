b6b0709e4b3dd452e9d085d4bf0dc1616e016ecc
Christoph Muellner <cmuellner@linux.com>
2025-01-21 11:45:42 +0100

    target/riscv: implement Zicbom extension
    Zicbom is the Cache-Block Management extension defined in the already
    ratified RISC-V Base Cache Management Operation (CBO) ISA extension [1].
    
    The extension contains three instructions: cbo.clean, cbo.flush and
    cbo.inval. All of them must be implemented in the same group as LQ and
    cbo.zero due to overlapping patterns.
    
    All these instructions can throw a Illegal Instruction/Virtual
    Instruction exception, similar to the existing cbo.zero. The same
    check_zicbo_envcfg() is used to handle these exceptions.
    
    Aside from that, these instructions also need to handle page faults and
    guest page faults. This is done in a new check_zicbom_access() helper.
    
    As with Zicboz, the cache block size for Zicbom is also configurable.
    Note that the spec determines that Zicbo[mp] and Zicboz can have
    different cache sizes (Section 2.7 of [1]), so we also include a
    'cbom_blocksize' to go along with the existing 'cboz_blocksize'. They
    are set to the same size, so unless users want to play around with the
    settings both sizes will be the same.
    
    [1] https://github.com/riscv/riscv-CMOs/blob/master/specifications/cmobase-v1.0.1.pdf
    
    Reviewed-by: Richard Henderson <richard.henderson@linaro.org>
    Reviewed-by: Weiwei Li <liweiwei@iscas.ac.cn>
    Co-developed-by: Philipp Tomsich <philipp.tomsich@vrull.eu>
    Signed-off-by: Christoph Muellner <cmuellner@linux.com>
    Signed-off-by: Daniel Henrique Barboza <dbarboza@ventanamicro.com>
    Message-ID: <20230224132536.552293-4-dbarboza@ventanamicro.com>
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    
    This is a manual backport of upstream commit e05da09b7cf ("target/riscv:
    implement Zicbom extension").
    
    Update the extension definition and properties for 6.x.
    cpu_gpr[] is not used for cheri, use get_gpr to read a register value.
    Explain the missing size parameter for probe_access_flags.

--- target/riscv/cpu.h
+++ target/riscv/cpu.h
@@ -478,6 +478,7 @@ struct RISCVCPU {
         bool ext_counters;
         bool ext_ifencei;
         bool ext_icsr;
+        bool ext_icbom;
         bool ext_icboz;
 #ifdef TARGET_CHERI
         bool ext_cheri_purecap; 
@@ -495,6 +496,7 @@ struct RISCVCPU {
         char *vext_spec;
         uint16_t vlen;
         uint16_t elen;
+        uint16_t cbom_blocksize;
         uint16_t cboz_blocksize;
         bool mmu;
         bool pmp;
